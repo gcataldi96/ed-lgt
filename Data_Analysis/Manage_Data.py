773bc185c6f9846ec870ea158179d12c47374a08
Giovanni Cataldi <giovacataldi96@gmail.com>
2022-06-11 10:16:55 +0000

    Formatting changes

--- Data_Analysis/Manage_Data.py
+++ Data_Analysis/Manage_Data.py
@@ -2,13 +2,14 @@ import numpy as np
 import pickle
 import os
 
-def store_results(data_file,x_data,new_data):
+
+def store_results(data_file, x_data, new_data):
     if not isinstance(data_file, str):
-        raise TypeError(f'data_file should be a STRING, not a {type(data_file)}')
+        raise TypeError(f"data_file should be a STRING, not a {type(data_file)}")
     if not isinstance(x_data, list):
-        raise TypeError(f'x_data must be a LIST, not a {type(x_data)}')
+        raise TypeError(f"x_data must be a LIST, not a {type(x_data)}")
     if not isinstance(new_data, list):
-        raise TypeError(f'new_data must be a LIST, not a {type(new_data)}')
+        raise TypeError(f"new_data must be a LIST, not a {type(new_data)}")
     """
     THIS FUNCTION STORES SOME VALUES IN NEW COLUMN OF A FILE
     WITH ALREADY EXISTING COLUMNS OF vocabulary. THESE WOULD
@@ -22,57 +23,59 @@ def store_results(data_file,x_data,new_data):
     """
     # STORE X VALUES
     if not os.path.exists(data_file):
-        g=open(data_file,'w+')
+        g = open(data_file, "w+")
         for ii in range(len(x_data)):
-            g.write(str(x_data[ii])+'\n')
+            g.write(str(x_data[ii]) + "\n")
         g.close()
     # STORE NEW Y VALUES
-    f=open(data_file,'r+')
-    line=f.readlines()
+    f = open(data_file, "r+")
+    line = f.readlines()
     f.close()
-    h=open(data_file,'w+')
+    h = open(data_file, "w+")
     for ii in range(len(line)):
-        line[ii]=line[ii].rstrip()
-        h.write(line[ii]+','+ str(new_data[ii])+'\n')
+        line[ii] = line[ii].rstrip()
+        h.write(line[ii] + "," + str(new_data[ii]) + "\n")
     h.close()
 
+
 def save_dictionary(dict, filename):
-    with open(filename, "wb") as outp:  # Overwrites any existing file.
+    with open(f"{filename}.pkl", "wb") as outp:  # Overwrites any existing file.
         pickle.dump(dict, outp, pickle.HIGHEST_PROTOCOL)
     outp.close
 
 
-def acquire_data(data_file_name,row_for_labels=False):
+def acquire_data(data_file_name, row_for_labels=False):
     if not isinstance(data_file_name, str):
-        raise TypeError(f'data_file_name should be a STRING, not a {type(data_file_name)}')
+        raise TypeError(
+            f"data_file_name should be a STRING, not a {type(data_file_name)}"
+        )
     if not isinstance(row_for_labels, bool):
-        raise TypeError(f'row_for_labels must be a BOOL, not a {type(row_for_labels)}')
+        raise TypeError(f"row_for_labels must be a BOOL, not a {type(row_for_labels)}")
     # Open the file and acquire all the lines
-    f=open(data_file_name,'r+')
-    line=f.readlines()
+    f = open(data_file_name, "r+")
+    line = f.readlines()
     f.close()
     # CREATE A DICTIONARY TO HOST THE LISTS OBTAINED FROM EACH COLUMN OF data_file
-    data={}
+    data = {}
     # Get the first line of the File as a list of entries.
-    n=line[0].strip().split(',')
-    for ii in range(0,len(n)):
+    n = line[0].strip().split(",")
+    for ii in range(0, len(n)):
         # Generate a list for each column of data_file
-        data[str(ii)]=list()
+        data[str(ii)] = list()
         if row_for_labels:
             # Generate a label for each list acquiring the ii+1 entry of the first line n
-            data['label_%s' % str(ii)]=str(n[ii])
+            data["label_%s" % str(ii)] = str(n[ii])
 
     if row_for_labels:
         # IGNORE THE FIRST LINE OF line (ALREAY USED FOR THE LABELS)
-        del line[0]     
+        del line[0]
 
     # Fill the lists with the entries of Columns
     for ii in range(len(line)):
-        a=line[ii].strip().split(',')
+        a = line[ii].strip().split(",")
 
         for jj in range(len(n)):
             data[str(jj)].append(float(a[jj]))
     for ii in range(len(n)):
-        data[str(ii)]=np.asarray(data[str(ii)])
+        data[str(ii)] = np.asarray(data[str(ii)])
     return data
-
