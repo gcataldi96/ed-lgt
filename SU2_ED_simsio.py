1f98b82120fbfa7768b192692e771b223270c224
Giovanni Cataldi <giovacataldi96@gmail.com>
2022-09-29 10:16:48 +0000

    rename local_dimension +comments

--- SU2_ED_simsio.py
+++ SU2_ED_simsio.py
@@ -1,16 +1,20 @@
 from simsio import run_sim, logger
 from scipy.sparse import csr_matrix
-
 from Error_Debugging.Checks import pause
-from Hamitonian_Functions.QMB_Operations.Density_Matrix_Tools import truncation
+from Hamitonian_Functions.QMB_Operations.Density_Matrix_Tools import (
+    truncation,
+    entanglement_entropy,
+)
 from Hamitonian_Functions.QMB_Operations.Density_Matrix_Tools import Pure_State
 from Hamitonian_Functions.LGT_Objects import *
 from Hamitonian_Functions.LGT_Hamiltonians import *
 from Hamitonian_Functions.Print_Observables import *
+from Hamitonian_Functions.Symmetry_sectors import *
 
 # ===================================================================================
 
 with run_sim() as sim:
+    # sim.link("psi")
     # X-Lattice dimension
     nx = sim.par["nx"]
     # Y-Lattice dimension
@@ -33,7 +37,7 @@ with run_sim() as sim:
         theory_label = "Pure"
         phrase = "     PURE THEORY: NO MATTER FIELDS"
         # Local dimension of the theory
-        local_dimension = 9
+        loc_dim = 9
         # IMPORTING SINGLE SITE OPERATORS
         from Operators.SU2_Free_Operators import identity
         from Operators.SU2_Free_Operators import gamma_operator
@@ -44,7 +48,7 @@ with run_sim() as sim:
         theory_label = "Full"
         phrase = "     FULL THEORY: MATTER + GAUGE FIELDS"
         # Local dimension of the theory
-        local_dimension = 30
+        loc_dim = 30
         # IMPORTING SINGLE SITE OPERATORS
         from Operators.SU2_Matter_Operators import identity
         from Operators.SU2_Matter_Operators import gamma_operator
@@ -186,14 +190,22 @@ with run_sim() as sim:
     # ----------------------------------------------------------------------------------
     # DIAGONALIZING THE HAMILTONIAN
     GS = Pure_State()
-    GS.ground_state(H, debug)
+    # GS.ground_state(H)
+    GS.get_first_n_eigs(H)
     # RESHIFT THE LOWEST EIGENVALUE OF THE HAMILTONIAN BY THE ENERGY PENALTIES
     # AND GET THE GROUND STATE ENERGY DENSITY (OF SINGLE SITE)
-    GS.energy = (GS.energy + n_penalties * eta) / n
+    GS.GSenergy = (GS.GSenergy + n_penalties * eta) / n
     # SAVE THE ENERGY VALUE in the dictionary
-    sim.res["energy"] = GS.energy
+    sim.res["energy"] = GS.N_energies
     # TRUNCATE THE ENTRIES OF THE GROUND STATE
     psi = truncation(GS.psi, 10 ** (-10))
+    # MASURE ENTANGLEMENT OF A BIPARTITION
+    sim.res["entropy"] = entanglement_entropy(
+        psi=psi, loc_dim=loc_dim, partition_size=int(nx * ny / 2)
+    )
+    # SAVE STATE
+    sim.res["psi"] = psi
+    # sim["psi"] = psi
     # ----------------------------------------------------------------------------------
     if not pure_theory:
         logger.info("----------------------------------------------------")
@@ -201,7 +213,7 @@ with run_sim() as sim:
     logger.info("----------------------------------------------------")
     logger.info(f"    SU(2) GAUGE COUPLING g {format(g, '.3f')}")
     logger.info("----------------------------------------------------")
-    logger.info(f"    GROUND STATE ENERGY  E {format(GS.energy, '.5f')}")
+    logger.info(f"    GROUND STATE ENERGY  E {GS.GSenergy}")
     logger.info("----------------------------------------------------")
     # COMPUTE GAMMA EXPECTATION VALUE
     sim.res["gamma"], Obs["gamma"] = get_LOCAL_Observable(GS.psi, nx, ny, Gamma)
@@ -264,4 +276,29 @@ with run_sim() as sim:
         print_Observables(Obs[key])
         logger.info("----------------------------------------------------")
         logger.info("")
-# nohup bash -c "printf 'n%s\n' {0..20} | shuf | xargs -P6 -i python scripts/dmrgs.py massless/phases/weak {} 8" &>/dev/null &
+
+
+"""phrase = "SYMMETRY SECTORS OF THE HAMILTONIAN"
+# GET THE SINGLE SITE SYMMETRY SECTORS WRT THE NUMBER OF FERMIONS
+N_tot_sectors = [0, 1, 2]
+N_tot_dim_sectors = [9, 12, 9]
+single_site_syms = single_site_symmetry_sectors(
+    loc_dim, sectors_list=N_tot_sectors, dim_sectors_list=N_tot_dim_sectors
+)
+# GET THE NBODY SYMMETRY SECTOR STATE
+Nbody_syms = many_body_symmetry_sectors(single_site_syms, n) - 4
+
+
+H_subsector = {}
+for ii in range(-4, 5, 2):
+    print(ii)
+    # GET THE INDICES ASSOCIATED TO EACH SYMMETRY SECTOR
+    indices = syms.get_indices_from_array(Nbody_syms, ii)
+    indices = indices.tolist()
+    print("Computing H subsector of ", ii)
+    H_subsector[str(ii)] = syms.get_submatrix_from_sparse(H, indices, indices)
+    sub_energy, sub_psi = get_ground_state_from_Hamiltonian(
+        csr_matrix(H_subsector[str(ii)]), debug=False
+    )
+    sub_energy = (sub_energy + n_penalties * eta) / n
+    print("SUBSECTOR", ii, "ENERGY", sub_energy)"""
