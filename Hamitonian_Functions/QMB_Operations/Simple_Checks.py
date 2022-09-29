3ec3f378fe9896ea7bfe7db902544cb29776f49b
Giovanni Cataldi <giovacataldi96@gmail.com>
2022-09-29 10:12:48 +0000

    Replace print with logger info from simsio

--- Hamitonian_Functions/QMB_Operations/Simple_Checks.py
+++ Hamitonian_Functions/QMB_Operations/Simple_Checks.py
@@ -1,53 +1,61 @@
-from scipy.sparse.base import isspmatrix
+from scipy.sparse import isspmatrix
 import numpy as np
+from simsio import logger
 from scipy.sparse.linalg import norm
+
 # =====================================================================================
-def pause(phrase,debug):
+def pause(phrase, debug):
     if debug == True:
         # IT PROVIDES A PAUSE (with a phrase) in a given point of the PYTHON CODE
-        print('----------------------------------------------------')
-        #Press the <ENTER> key to continue
-        programPause = input(phrase)        
-        print('----------------------------------------------------')
-        print('')
+        logger.info("----------------------------------------------------")
+        # Press the <ENTER> key to continue
+        programPause = input(phrase)
+        logger.info("----------------------------------------------------")
+        logger.info("")
+
 
 # =====================================================================================
-def alert(phrase,debug):
+def alert(phrase, debug):
     if debug == True:
         # IT PRINTS A PHRASE IN A GIVEN POINT OF A PYTHON CODE
-        print('')
-        print(phrase)
+        logger.info("")
+        logger.info(phrase)
+
 
 # =====================================================================================
-def check_commutator(A,B):
+def check_commutator(A, B):
     # THIS FUNCTION CHECK THE COMMUTATION RELATIONS BETWEEN THE OPERATORS A AND B
     if not isspmatrix(A):
-        raise TypeError(f'A should be a csr_matrix, not a {type(A)}')
+        raise TypeError(f"A should be a csr_matrix, not a {type(A)}")
     if not isspmatrix(B):
-        raise TypeError(f'B should be a csr_matrix, not a {type(B)}')
-    norma=norm(A*B-B*A)
-    norma_max=max(norm(A*B+B*A),norm(A),norm(B))
-    ratio=norma/norma_max
-    #check=(AB!=BA).nnz
-    if ratio>10**(-15):
-        print('    ERROR: A and B do NOT COMMUTE')
-        print('    NORM', norma)
-        print('    RATIO', ratio)
-    print('')
+        raise TypeError(f"B should be a csr_matrix, not a {type(B)}")
+    norma = norm(A * B - B * A)
+    norma_max = max(norm(A * B + B * A), norm(A), norm(B))
+    ratio = norma / norma_max
+    # check=(AB!=BA).nnz
+    if ratio > 10 ** (-15):
+        logger.info("    ERROR: A and B do NOT COMMUTE")
+        logger.info("    NORM", norma)
+        logger.info("    RATIO", ratio)
+    logger.info("")
+
+
 # =====================================================================================
-def check_matrix(A,B):
+def check_matrix(A, B):
     # THIS FUNCTION CHECK THE DIFFERENCE BETWEEN TWO SPARSE MATRICES
     # CHECK TYPE
     if not isspmatrix(A):
-        raise TypeError(f'A should be a csr_matrix, not a {type(A)}')
+        raise TypeError(f"A should be a csr_matrix, not a {type(A)}")
     if not isspmatrix(B):
-        raise TypeError(f'B should be a csr_matrix, not a {type(B)}')
-    norma=norm(A-B)
-    norma_max=max(norm(A+B),norm(A),norm(B))
-    ratio=norma/norma_max
-    if ratio>10**(-15):
-        print('    ERROR: A and B are DIFFERENT MATRICES')
-        print('    NORM', norma)
-        print('    RATIO', ratio)
-    print('')
+        raise TypeError(f"B should be a csr_matrix, not a {type(B)}")
+    norma = norm(A - B)
+    norma_max = max(norm(A + B), norm(A), norm(B))
+    ratio = norma / norma_max
+    if ratio > 10 ** (-15):
+        logger.info("    ERROR: A and B are DIFFERENT MATRICES")
+        logger.info("    NORM", norma)
+        logger.info("    RATIO", ratio)
+    logger.info("")
+
+
 # =====================================================================================
