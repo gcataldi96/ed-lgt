45e1f2991d189f461ef28d556cbb28864db6cf3c
gcataldi96 <giovacataldi96@gmail.com>
2022-11-16 10:48:37 +0000

    update init file with new modules

--- modeling/__init__.py
+++ modeling/__init__.py
@@ -1,11 +1,13 @@
-from . import baseterm, localterm, twobodyterm2d, plaquetteterm2d
-
-from .baseterm import *
-from .localterm import *
-from .twobodyterm2d import *
-from .plaquetteterm2d import *
+from . import local_term, plaquette_term2d, twobody_term2d, qmb_operations, qmb_state
+from .local_term import *
+from .plaquette_term2d import *
+from .twobody_term2d import *
+from .qmb_operations import *
+from .qmb_state import *
 
 # All modules have an __all__ defined
-__all__ = localterm.__all__.copy()
-__all__ += twobodyterm2d.__all__.copy()
-__all__ += plaquetteterm2d.__all__.copy()
+__all__ = local_term.__all__.copy()
+__all__ += twobody_term2d.__all__.copy()
+__all__ += plaquette_term2d.__all__.copy()
+__all__ += qmb_operations.__all__.copy()
+__all__ += qmb_state.__all__.copy()
