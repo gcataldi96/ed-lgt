9ac5d3b3ed202f7ddc633591ac0d06fd644ae43f
gcataldi96 <giovacataldi96@gmail.com>
2022-11-29 10:58:33 +0000

    update names of submodule in modeling

--- modeling/__init__.py
+++ modeling/__init__.py
@@ -1,13 +1,15 @@
-from . import local_term, plaquette_term2d, twobody_term2d, qmb_operations, qmb_state
+from . import local_term, plaquette_term, twobody_term, qmb_operations, qmb_state
 from .local_term import *
-from .plaquette_term2d import *
-from .twobody_term2d import *
+from .qmb_operations import *
+from .plaquette_term import *
+from .twobody_term import *
 from .qmb_operations import *
 from .qmb_state import *
 
 # All modules have an __all__ defined
 __all__ = local_term.__all__.copy()
-__all__ += twobody_term2d.__all__.copy()
-__all__ += plaquette_term2d.__all__.copy()
+__all__ += qmb_operations.__all__.copy()
+__all__ += twobody_term.__all__.copy()
+__all__ += plaquette_term.__all__.copy()
 __all__ += qmb_operations.__all__.copy()
 __all__ += qmb_state.__all__.copy()
