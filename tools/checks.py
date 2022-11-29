54c53cde5020ab0ef3f35cb8705ec7509a6b6a34
gcataldi96 <giovacataldi96@gmail.com>
2022-11-29 10:59:23 +0000

    temporarily use print instead logger. This will be restored once the code is ready

--- tools/checks.py
+++ tools/checks.py
@@ -5,6 +5,7 @@ from scipy.sparse.linalg import norm
 
 __all__ = ["pause", "alert", "check_commutator", "check_matrix"]
 
+
 def pause(phrase, debug):
     if not isinstance(phrase, str):
         raise TypeError(f"phrase should be a STRING, not a {type(phrase)}")
@@ -12,11 +13,12 @@ def pause(phrase, debug):
         raise TypeError(f"debug should be a BOOL, not a {type(debug)}")
     if debug == True:
         # IT PROVIDES A PAUSE (with a phrase) in a given point of the PYTHON CODE
-        logger.info("----------------------------------------------------")
+        print("----------------------------------------------------")
         # Press the <ENTER> key to continue
         programPause = input(phrase)
-        logger.info("----------------------------------------------------")
-        logger.info("")
+        print("----------------------------------------------------")
+        print("")
+
 
 def alert(phrase, debug):
     if not isinstance(phrase, str):
@@ -25,8 +27,9 @@ def alert(phrase, debug):
         raise TypeError(f"debug should be a BOOL, not a {type(debug)}")
     if debug == True:
         # IT PRINTS A PHRASE IN A GIVEN POINT OF A PYTHON CODE
-        logger.info("")
-        logger.info(phrase)
+        print("")
+        print(phrase)
+
 
 def check_commutator(A, B):
     # THIS FUNCTION CHECK THE COMMUTATION RELATIONS BETWEEN THE OPERATORS A AND B
@@ -39,10 +42,11 @@ def check_commutator(A, B):
     ratio = norma / norma_max
     # check=(AB!=BA).nnz
     if ratio > 10 ** (-15):
-        logger.info("    ERROR: A and B do NOT COMMUTE")
-        logger.info("    NORM", norma)
-        logger.info("    RATIO", ratio)
-    logger.info("")
+        print("    ERROR: A and B do NOT COMMUTE")
+        print("    NORM", norma)
+        print("    RATIO", ratio)
+    print("")
+
 
 def check_matrix(A, B):
     # THIS FUNCTION CHECK THE DIFFERENCE BETWEEN TWO SPARSE MATRICES
@@ -51,11 +55,16 @@ def check_matrix(A, B):
         raise TypeError(f"A should be a csr_matrix, not a {type(A)}")
     if not isspmatrix(B):
         raise TypeError(f"B should be a csr_matrix, not a {type(B)}")
+    if A.shape != B.shape:
+        raise ValueError(
+            f"A and B should have the same shape but A is {A.shape}, B is {B.shape}"
+        )
     norma = norm(A - B)
     norma_max = max(norm(A + B), norm(A), norm(B))
     ratio = norma / norma_max
     if ratio > 10 ** (-15):
-        logger.info("    ERROR: A and B are DIFFERENT MATRICES")
-        logger.info("    NORM", norma)
-        logger.info("    RATIO", ratio)
-    logger.info("")
+        print("    ERROR: A and B are DIFFERENT MATRICES")
+        print("    NORM", norma)
+        print("    RATIO", ratio)
+    else:
+        print("The two matrices are equal")
