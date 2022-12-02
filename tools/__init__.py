9ddad4792ac77a4a69d4ef5909da2b2a7862114e
gcataldi96 <giovacataldi96@gmail.com>
2022-12-02 00:50:27 +0000

    add LGT_analysis

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,11 +1,13 @@
-from . import checks, derivatives, mappings_1D_2D, manage_data
+from . import checks, derivatives, mappings_1D_2D, manage_data, LGT_analysis
 from .checks import *
 from .derivatives import *
 from .mappings_1D_2D import *
 from .manage_data import *
+from .LGT_analysis import *
 
 # All modules have an __all__ defined
 __all__ = mappings_1D_2D.__all__.copy()
 __all__ += checks.__all__.copy()
 __all__ += derivatives.__all__.copy()
 __all__ += manage_data.__all__.copy()
+__all__ += LGT_analysis.__all__.copy()
