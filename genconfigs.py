b437585fed3c9ed8fa4b96c6e0da1c9447596a80
gcataldi96 <giovacataldi96@gmail.com>
2022-12-06 22:31:59 +0000

    update pure and full ED configurations

--- genconfigs.py
+++ genconfigs.py
@@ -1,8 +1,19 @@
+# %%
 from simsio import gen_configs
 import numpy as np
 
-params = {
-    "m": np.logspace(-4, 0, 10),
-    "g": np.logspace(-1, 1, 20),
-}
-gen_configs("template", params, "deltaN2_grid")
+# %%
+# GENERATE SIMULATION PARAMETERS FOR PURE THEORY
+params = {"g": np.logspace(-1, 1, 20)}
+for L in [2, 3, 4]:
+    for bc in ["OBC", "PBC"]:
+        gen_configs("template", params, f"pure_{L}x2_{bc}")
+
+# %%
+# GENERATE SIMULATION PARAMETERS FOR FULL THEORY
+params = {"g": np.logspace(-1, 1, 20), "m": np.logspace(-2, 0, 10)}
+for N in [0, 2]:
+    for bc in ["OBC", "PBC"]:
+        gen_configs("template", params, f"full_Delta{N}_{bc}")
+
+# %%
