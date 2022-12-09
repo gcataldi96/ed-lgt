50f625f43cec3185b65650b83f2e9e158711f4ba
gcataldi96 <giovacataldi96@gmail.com>
2022-12-09 08:38:17 +0000

    remove __all__ functions from init (old script modules)

--- Data_Analysis/__init__.py
+++ Data_Analysis/__init__.py
@@ -1,7 +1,3 @@
 from . import Derivatives, Manage_Data
 from .Derivatives import *
 from .Manage_Data import *
-
-# All modules have an __all__ defined
-__all__ = Derivatives.__all__.copy()
-__all__ += Manage_Data.__all__.copy()
--- Error_Debugging/__init__.py
+++ Error_Debugging/__init__.py
@@ -1,5 +1,2 @@
 from . import Checks
 from .Checks import *
-
-# All modules have an __all__ defined
-__all__ = Checks.__all__.copy()
