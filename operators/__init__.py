8d9adb8e22598c337117316035cb8d80cc1c6084
Giovanni <giovacataldi96@gmail.com>
2023-03-12 15:08:15 +0000

    rename spin operators file

--- operators/__init__.py
+++ operators/__init__.py
@@ -1,11 +1,11 @@
-from . import su2_operators, bose_fermi_operators, spin12_operators, qed_operators
+from . import spin_operators, su2_operators, bose_fermi_operators, qed_operators
 from .su2_operators import *
 from .qed_operators import *
 from .bose_fermi_operators import *
-from .spin12_operators import *
+from .spin_operators import *
 
 # All modules have an __all__ defined
 __all__ = su2_operators.__all__.copy()
 __all__ += qed_operators.__all__.copy()
 __all__ += bose_fermi_operators.__all__.copy()
-__all__ += spin12_operators.__all__.copy()
+__all__ += spin_operators.__all__.copy()
