e53cfe69ed2472533ae566ea5f62283be4f80c83
Giovanni <giovacataldi96@gmail.com>
2023-05-06 09:32:21 +0000

    update topology parameters for full theory

--- genconfigs.py
+++ genconfigs.py
@@ -9,10 +9,6 @@ for L in [2, 3, 4]:
     for bc in ["OBC", "PBC"]:
         gen_configs("template", params, f"pure_{L}x2_{bc}")
 # %%
-# PURE THEORY TOPOLOGY
-params = {"g": np.logspace(-1, 1, 15)}
-gen_configs("template", params, f"pure_topology")
-# %%
 # PARAMETERS FOR FULL THEORY
 params = {"g": np.logspace(-1, 1, 20), "m": np.logspace(-1, 0, 10)}
 for N in [0, 2]:
@@ -23,8 +19,14 @@ for N in [0, 2]:
 params = {"DeltaN": np.array([0, -4, -2, 2, 4]), "g": np.logspace(-1, 1, 20)}
 gen_configs("template", params, f"full_sym_sectors")
 # %%
-# FULL THEORY TOPOLOGY
-params = {"g": np.logspace(-1, 1, 15), "m": np.array([1e-4, 1e-3, 1e-2, 1e-1, 1, 1e1])}
-gen_configs("template", params, f"full_topology")
-
+# PURE THEORY TOPOLOGY
+params = {"g": np.logspace(-1, 1, 15)}
+gen_configs("template", params, f"pure_topology")
 # %%
+# FULL THEORY TOPOLOGY pt1
+params = {"g": np.logspace(-1, 1, 10), "m": np.array([1e-2, 1e-1, 1, 1e1, 1e2])}
+gen_configs("template", params, f"SU2/full/topology1")
+# %%
+# FULL THEORY TOPOLOGY pt2
+params = {"g": np.logspace(-1, 1, 5), "m": np.logspace(-2, 4, 7)}
+gen_configs("template", params, f"SU2/full/topology2")
