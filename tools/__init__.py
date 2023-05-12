a4036b56c8dad8a65d5720c21bc3fb8ebac959a9
Giovanni <giovacataldi96@gmail.com>
2023-05-12 07:19:49 +0000

    add measure of structure factor

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,9 +1,10 @@
-from . import checks, derivatives, mappings_1D_2D, manage_data, LGT_analysis
+from . import checks, derivatives, mappings_1D_2D, manage_data, LGT_analysis, measures
 from .checks import *
 from .derivatives import *
 from .mappings_1D_2D import *
 from .manage_data import *
 from .LGT_analysis import *
+from .measures import *
 
 # All modules have an __all__ defined
 __all__ = mappings_1D_2D.__all__.copy()
@@ -11,3 +12,4 @@ __all__ += checks.__all__.copy()
 __all__ += derivatives.__all__.copy()
 __all__ += manage_data.__all__.copy()
 __all__ += LGT_analysis.__all__.copy()
+__all__ += measures.__all__.copy()
