9565938a307af22036af3805c5a41e707808944d
Giovanni <giovacataldi96@gmail.com>
2023-05-13 10:39:36 +0000

    add analysis for SU2 phase diagram

--- script_Analysis.py
+++ script_Analysis.py
@@ -2,7 +2,7 @@
 import numpy as np
 from simsio import *
 from matplotlib import pyplot as plt
-from tools import save_dictionary
+from tools import save_dictionary, get_charge, get_density, structure_factor
 
 """
 To extract simulations use
@@ -13,6 +13,12 @@ To acquire the psi file
     sim.link("psi")
     psi= sim.load("psi", cache=True)
 """
+
+
+@plt.FuncFormatter
+def fake_log(x, pos):
+    "The two args are the value and tick position"
+    return r"$10^{%d}$" % (x)
 
 
 def get_obs_list(model, pure=None, has_obc=True):
@@ -115,4 +121,49 @@ for ii, g in enumerate(res["g"]):
     plt.legend()
     plt.ylabel("1-py_sector")
 save_dictionary(res, "saved_dicts/SU2_full_topology2.pkl")
+# %%
+# SU2 PHASE DIAGRAM
+config_filename = "SU2/full/phase_diagram"
+match = SimsQuery(group_glob=config_filename)
+ugrid, vals = uids_grid(match.uids, ["g", "m"])
+obs_list = get_obs_list(model="SU2", pure=False, has_obc=False)
+res = {"g": vals["g"], "m": vals["m"]}
+for obs in obs_list:
+    res[obs] = np.zeros((res["g"].shape[0], res["m"].shape[0]))
+    for ii, g in enumerate(res["g"]):
+        for jj, m in enumerate(res["m"]):
+            res[obs][ii][jj] = get_sim(ugrid[ii][jj]).res[obs]
+
+fig, axs = plt.subplots(
+    2,
+    1,
+    sharex=True,
+    sharey=True,
+    constrained_layout=True,
+)
+obs = ["E_square", "n_tot_even"]
+for ii, ax in enumerate(axs.flat):
+    # IMSHOW
+    img = ax.imshow(
+        np.transpose(res[obs[ii]]),
+        origin="lower",
+        cmap="magma",
+        extent=[-2, 2, -3, 1],
+    )
+    ax.set_ylabel(r"m")
+    axs[1].set_xlabel(r"g2")
+    ax.set(xticks=[-2, -1, 0, 1, 2], yticks=[-3, -2, -1, 0, 1])
+    ax.xaxis.set_major_formatter(fake_log)
+    ax.yaxis.set_major_formatter(fake_log)
+
+    cb = fig.colorbar(
+        img,
+        ax=ax,
+        aspect=20,
+        location="right",
+        orientation="vertical",
+        pad=0.01,
+        label=obs[ii],
+    )
+save_dictionary(res, "saved_dicts/SU2_full_phase_diagram.pkl")
 # %%
