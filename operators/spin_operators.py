bf1339000d96c7f0b2e5b1fff5da17c2094bb635
Giovanni <giovacataldi96@gmail.com>
2023-05-15 21:57:08 +0000

    define spin operators in any spin rep

--- operators/spin_operators.py
+++ operators/spin_operators.py
@@ -1,23 +1,35 @@
 import numpy as np
-from scipy.sparse import csr_matrix
+from scipy.sparse import diags
 
-__all__ = ["get_spin12_operators"]
+__all__ = ["get_spin_operators"]
 
 
-def get_spin12_operators():
+def get_spin_operators(s):
+    if not np.isscalar(s):
+        raise TypeError(f"s must be SCALAR & (semi)INTEGER, not {type(s)}")
+    """
+    This function computes the spin (sparse) matrices: 
+    [Sz, Sp=S+, Sm=S-, Sx, Sy, S2=Casimir]
+    in any arbitrary spin-s representation
+
+    Args:
+        s (scalar, real): spin value, assumed to be integer or semi-integer
+
+    Returns:
+        dict: dictionary with the spin matrices
+    """
+    # Size of the spin matrix
+    size = int(2 * s + 1)
+    shape = (size, size)
+    # Diagonal entries of the Sz matrix
+    sz_diag = np.arange(-s, s + 1)[::-1]
+    # Diagonal entries of the S+ matrix
+    sp_diag = np.sqrt(s * (s + 1) - sz_diag[1:] * (sz_diag[1:] + 1))
     ops = {}
-    data_sx = np.array([1, 1])
-    x_sx = np.array([1, 2])
-    y_sx = np.array([2, 1])
-    ops["sigma_x"] = csr_matrix((data_sx, (x_sx - 1, y_sx - 1)), shape=(2, 2))
-    # -------------------------------------------------------
-    data_sy = np.array([complex(0, -1), complex(0, 1)])
-    x_sy = np.array([1, 2])
-    y_sy = np.array([2, 1])
-    ops["sigma_y"] = csr_matrix((data_sy, (x_sy - 1, y_sy - 1)), shape=(2, 2))
-    # -------------------------------------------------------
-    data_sz = np.array([1, -1])
-    x_sz = np.array([1, 2])
-    y_sz = np.array([1, 2])
-    ops["sigma_z"] = csr_matrix((data_sz, (x_sz - 1, y_sz - 1)), shape=(2, 2))
+    ops["Sz"] = diags(sz_diag, 0, shape)
+    ops["Sp"] = diags(sp_diag, 1, shape)
+    ops["Sm"] = ops["Sp"].transpose()
+    ops["Sx"] = 0.5 * (ops["Sp"] + ops["Sm"])
+    ops["Sy"] = complex(0, -0.5) * (ops["Sp"] - ops["Sm"])
+    ops["S2"] = diags([s * (s + 1) for i in range(size)], 0, shape)
     return ops
