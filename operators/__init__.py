0d8529eadaab38b5362d2e662ba82bf5c44d9eae
Giovanni <giovacataldi96@gmail.com>
2023-07-25 16:50:32 +0000

    include SU2 new operators

--- operators/__init__.py
+++ operators/__init__.py
@@ -1,11 +1,13 @@
 from . import (
     spin_operators,
     su2_operators,
+    SU2_operators,
     bose_fermi_operators,
     qed_operators,
     z2_operators,
 )
 from .su2_operators import *
+from .SU2_operators import *
 from .qed_operators import *
 from .bose_fermi_operators import *
 from .spin_operators import *
@@ -13,6 +15,7 @@ from .z2_operators import *
 
 # All modules have an __all__ defined
 __all__ = su2_operators.__all__.copy()
+__all__ += SU2_operators.__all__.copy()
 __all__ += qed_operators.__all__.copy()
 __all__ += bose_fermi_operators.__all__.copy()
 __all__ += spin_operators.__all__.copy()
