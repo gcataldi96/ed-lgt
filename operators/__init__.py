393fb3eed6967ed2ad820bbd44883aff402b1cde
Giovanni <giovacataldi96@gmail.com>
2023-07-25 16:52:43 +0000

    remove spin_operators

--- operators/__init__.py
+++ operators/__init__.py
@@ -1,5 +1,4 @@
 from . import (
-    spin_operators,
     su2_operators,
     SU2_operators,
     bose_fermi_operators,
@@ -10,7 +9,6 @@ from .su2_operators import *
 from .SU2_operators import *
 from .qed_operators import *
 from .bose_fermi_operators import *
-from .spin_operators import *
 from .z2_operators import *
 
 # All modules have an __all__ defined
@@ -18,5 +16,4 @@ __all__ = su2_operators.__all__.copy()
 __all__ += SU2_operators.__all__.copy()
 __all__ += qed_operators.__all__.copy()
 __all__ += bose_fermi_operators.__all__.copy()
-__all__ += spin_operators.__all__.copy()
 __all__ += z2_operators.__all__.copy()
