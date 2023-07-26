1f8d3175bb4cb665c8af2ac5f047b5cf6b4ef3c6
Giovanni <giovacataldi96@gmail.com>
2023-07-26 15:05:41 +0000

    add SU2_singlets module

--- operators/__init__.py
+++ operators/__init__.py
@@ -1,12 +1,14 @@
 from . import (
     su2_operators,
     SU2_operators,
+    SU2_singlets,
     bose_fermi_operators,
     qed_operators,
     z2_operators,
 )
 from .su2_operators import *
 from .SU2_operators import *
+from .SU2_singlets import *
 from .qed_operators import *
 from .bose_fermi_operators import *
 from .z2_operators import *
@@ -14,6 +16,7 @@ from .z2_operators import *
 # All modules have an __all__ defined
 __all__ = su2_operators.__all__.copy()
 __all__ += SU2_operators.__all__.copy()
+__all__ += SU2_singlets.__all__.copy()
 __all__ += qed_operators.__all__.copy()
 __all__ += bose_fermi_operators.__all__.copy()
 __all__ += z2_operators.__all__.copy()
