990b2d5b299d28bc2a81ada5648f4ede625dd1c9
Giovanni <giovacataldi96@gmail.com>
2023-08-03 07:30:03 +0000

    update old SU2 script

--- script_SU2_new.py
+++ script_SU2_new.py
@@ -14,6 +14,7 @@ from modeling import (
     diagonalize_density_matrix,
     get_state_configurations,
     truncation,
+    define_measurements,
 )
 from tools import get_energy_density, check_hermitian
 from simsio import logger, run_sim
@@ -35,21 +36,26 @@ with run_sim() as sim:
     # GET g COUPLING
     g = sim.par["g"]
     if pure_theory:
-        loc_dim = 9
         m = None
     else:
-        loc_dim = 30
         DeltaN = sim.par["DeltaN"]
         m = sim.par["m"]
     # ACQUIRE OPERATORS AS CSR MATRICES IN A DICTIONARY
     ops = SU2_dressed_site_operators(spin, pure_theory)
     # ACQUIRE OPERATORS AS CSR MATRICES IN A DICTIONARY
-    M, _ = SU2_gauge_basis(spin, pure_theory)
+    M, states = SU2_gauge_basis(spin, pure_theory)
     # ACQUIRE LOCAL DIMENSION OF EVERY SINGLE SITE
     lattice_base, loc_dims = lattice_base_configs(M, lvals, has_obc)
     loc_dims = loc_dims.transpose().reshape(n_sites)
     lattice_base = lattice_base.transpose().reshape(n_sites)
     logger.info(loc_dims)
+    """    
+    logger.info(lattice_base)
+    for label in lattice_base:
+        logger.info(f"label:{label}  dim: {len(states[label])}")
+        for s in states[label]:
+            s.show()
+    """
     # ACQUIRE HAMILTONIAN COEFFICIENTS
     coeffs = SU2_Hamiltonian_couplings(pure_theory, g, m)
     logger.info(f"PENALTY {coeffs['eta']}")
@@ -60,6 +66,7 @@ with run_sim() as sim:
     h_terms["E_square"] = LocalTerm2D(ops["E_square"], "E_square", site_basis=M)
     H += h_terms["E_square"].get_Hamiltonian(lvals, has_obc, coeffs["E"])
     # -------------------------------------------------------------------------------
+    """
     # LINK PENALTIES & Border penalties
     for d in directions:
         op_name_list = [f"T2_p{d}", f"T2_m{d}"]
@@ -67,7 +74,7 @@ with run_sim() as sim:
         # Define the Hamiltonian term
         h_terms[f"W_{d}"] = TwoBodyTerm2D(d, op_list, op_name_list, site_basis=M)
         H += h_terms[f"W_{d}"].get_Hamiltonian(
-            lvals, strength=2 * coeffs["eta"], has_obc=has_obc
+            lvals, strength=-2 * coeffs["eta"], has_obc=has_obc
         )
         # SINGLE SITE OPERATORS needed for the LINK SYMMETRY/OBC PENALTIES
         for s in "mp":
@@ -76,6 +83,7 @@ with run_sim() as sim:
             H += h_terms[op_name].get_Hamiltonian(
                 lvals=lvals, has_obc=has_obc, strength=coeffs["eta"]
             )
+            
     if not pure_theory:
         # STAGGERED MASS TERM
         h_terms["mass"] = LocalTerm2D(ops["N_tot"], "N_tot", site_basis=M)
@@ -98,7 +106,7 @@ with run_sim() as sim:
                 )
                 ** 2
             )
-    """
+
     # MAGNETIC ENERGY
     op_name_list = ["C_py_px", "C_py_mx", "C_my_px", "C_my_mx"]
     op_list = [ops[op] for op in op_name_list]
@@ -106,63 +114,66 @@ with run_sim() as sim:
     H += h_terms["plaq"].get_Hamiltonian(
         lvals, strength=coeffs["B"], has_obc=has_obc, add_dagger=True
     )
+    """
     # -----------------------------------------------------------------------------
     if not pure_theory:
         # HOPPING ACTIVITY
         for d in directions:
             for site in ["even", "odd"]:
-                op_name_list = [f"Q_p{d}_dag", f"Q_m{d}"]
-                op_list = [ops[op] for op in op_name_list]
-                h_terms[f"{d}_hop_{site}"] = TwoBodyTerm2D(d, op_list, op_name_list)
-                H += h_terms[f"{d}_hop_{site}"].get_Hamiltonian(
-                    lvals,
-                    strength=coeffs[f"t{d}_{site}"],
-                    has_obc=has_obc,
-                    add_dagger=True,
-                    mask=staggered_mask(lvals, site),
-                )
-    """
+                for ii, (k1, k2) in enumerate([("a", "b"), ("b", "a")]):
+                    sign = (-1) ** ii
+                    op_name_list = [f"Q{k1}_p{d}_dag", f"Q{k2}_m{d}"]
+                    op_list = [ops[op] for op in op_name_list]
+                    # DEFINE THE HAMILTONIAN TERM
+                    label_term = f"{d}_hop_{site}_{k1}{k2}"
+                    h_terms[label_term] = TwoBodyTerm2D(
+                        d, op_list, op_name_list, site_basis=M
+                    )
+                    H += h_terms[label_term].get_Hamiltonian(
+                        lvals,
+                        strength=sign * coeffs[f"t{d}_{site}"],
+                        has_obc=has_obc,
+                        add_dagger=True,
+                        mask=staggered_mask(lvals, site),
+                    )
+    print(H)
     # CHECK THAT THE HAMILTONIAN IS HERMITIAN
     check_hermitian(H)
     # DIAGONALIZE THE HAMILTONIAN
     n_eigs = sim.par["n_eigs"]
     GS = Ground_State(H, n_eigs)
-    sim.res["energy"] = GS.Nenergies
-    sim.res["energy_density"] = []
-    # ===========================================================================
-    # DEFINE THE OBSERVABLE LIST
-    sim.res["entropy"] = []
-    if not has_obc:
-        sim.res["rho_eigvals"] = []
     # ===========================================================================
     # LIST OF OBSERVABLES
-    obs_list = [f"T2_{s}{d}" for s in "mp" for d in directions] + ["E_square"]
-    for obs in obs_list:
+    gauge_obs = [f"T2_{s}{d}" for s in "mp" for d in directions] + ["E_square"]
+    for obs in gauge_obs:
         h_terms[obs] = LocalTerm2D(ops[obs], obs, site_basis=M)
-        sim.res[obs] = []
-    if not pure_theory:
+    if pure_theory:
+        matter_obs = None
+    else:
         matter_obs = ["N_up", "N_down", "N_tot", "N_single", "N_pair"]
         for obs in matter_obs:
             h_terms[obs] = LocalTerm2D(ops[obs], obs, site_basis=M)
-            sim.res[obs] = []
-            obs_list.append(obs)
+    # DEFINE THE LIST OF MEASUREMENTS
+    sim.res |= define_measurements(
+        obs_list=gauge_obs, stag_obs_list=matter_obs, has_obc=has_obc
+    )
+    sim.res["energy"] = GS.Nenergies
     # ===========================================================================
     # COMPUTE THE OBSERVABLES FOR EACH EIGENSTATES
     for ii in range(n_eigs):
-        logger.info("====================================================")
-        logger.info(f"{ii} ENERGY: {format(sim.res['energy'][ii], '.9f')}")
         # GET AND RESCALE SINGLE SITE ENERGIES
-        sim.res["energy_density"].append(
-            get_energy_density(
-                GS.Nenergies[ii],
-                lvals,
-                penalty=coeffs["eta"],
-                border_penalty=False,
-                link_penalty=True,
-                plaquette_penalty=False,
-                has_obc=has_obc,
-            )
+        energy_density = get_energy_density(
+            GS.Nenergies[ii],
+            lvals,
+            penalty=coeffs["eta"],
+            border_penalty=False,
+            link_penalty=False,
+            plaquette_penalty=False,
+            has_obc=has_obc,
         )
+        sim.res["energy_density"].append(energy_density)
+        logger.info("====================================================")
+        logger.info(f"{ii} ENERGY: {format(sim.res['energy_density'][ii], '.9f')}")
         # ENTROPY of a BIPARTITION
         sim.res["entropy"].append(
             entanglement_entropy(
@@ -171,8 +182,10 @@ with run_sim() as sim:
         )
         if has_obc:
             # GET STATE CONFIGURATIONS
-            get_state_configurations(
-                truncation(GS.Npsi[:, ii], 1e-10), loc_dims, n_sites
+            sim.res["state_configurations"].append(
+                get_state_configurations(
+                    truncation(GS.Npsi[:, ii], 1e-15), loc_dims, n_sites
+                )
             )
         else:
             # COMPUTE THE REDUCED DENSITY MATRIX
@@ -185,16 +198,22 @@ with run_sim() as sim:
         # ===========================================================================
         # MEASURE GAUGE OBSERVABLES: RISHON CASIMIR OPERATORS, ELECTRIC ENERGY E^{2}
         # ===========================================================================
-        for obs in obs_list:
+        for obs in gauge_obs:
             h_terms[obs].get_expval(GS.Npsi[:, ii], lvals, has_obc)
             sim.res[obs].append(h_terms[obs].avg)
+            sim.res[f"delta_{obs}"].append(h_terms[obs].std)
         # ===========================================================================
         # COMPUTE MATTER OBSERVABLES (STAGGERED)
         # ===========================================================================
         if not pure_theory:
-            for obs in matter_obs:
-                h_terms[obs].get_expval(GS.Npsi[:, ii], lvals, has_obc)
+            for site in ["even", "odd"]:
+                for obs in matter_obs:
+                    h_terms[obs].get_expval(GS.Npsi[:, ii], lvals, has_obc, site=site)
+                    sim.res[f"{obs}_{site}"].append(h_terms[obs].avg)
+                    sim.res[f"delta_{obs}_{site}"].append(h_terms[obs].std)
         logger.info("====================================================")
-
+    # ===========================================================================
+    # ENERGY GAP
+    # ===========================================================================
     if n_eigs > 1:
         sim.res["DeltaE"] = np.abs(sim.res["energy"][0] - sim.res["energy"][1])
