966e6006effea864757f5fbe26e453ca6add91a8
Giovanni Cataldi <giovacataldi96@gmail.com>
2023-08-05 18:27:25 +0200

    rename old modules

--- operators/__init__.py
+++ operators/__init__.py
@@ -1,12 +1,12 @@
 from . import (
-    su2_operators,
+    SU2_operators_old,
     SU2_operators,
     SU2_singlets,
     bose_fermi_operators,
     qed_operators,
     z2_operators,
 )
-from .su2_operators import *
+from .SU2_operators_old import *
 from .SU2_operators import *
 from .SU2_singlets import *
 from .qed_operators import *
@@ -14,7 +14,7 @@ from .bose_fermi_operators import *
 from .z2_operators import *
 
 # All modules have an __all__ defined
-__all__ = su2_operators.__all__.copy()
+__all__ = SU2_operators_old.__all__.copy()
 __all__ += SU2_operators.__all__.copy()
 __all__ += SU2_singlets.__all__.copy()
 __all__ += qed_operators.__all__.copy()
