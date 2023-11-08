df14923a8c9dddd527e9c5a0f1a0aa91417989db
Giovanni Cataldi <giovacataldi96@gmail.com>
2023-11-08 15:05:40 +0100

    update fermi operators including spinless case

--- operators/bose_fermi_operators.py
+++ operators/bose_fermi_operators.py
@@ -1,52 +1,55 @@
 import numpy as np
-from scipy.sparse import csr_matrix
+from scipy.sparse import csr_matrix, diags
 from scipy.sparse import identity
+from modeling import qmb_operator as qmb_op
 
-__all__=["get_fermi_hubbard_operators", "get_bose_hubbard_operators"]
-
-def get_fermi_hubbard_operators():
-    data_c_UP=np.array([1.,1.])
-    x_c_UP=np.array([3,4])
-    y_c_UP=np.array([1,2])
-    c_UP=csr_matrix((data_c_UP,(x_c_UP-1,y_c_UP-1)),shape=(4,4))
-    # ---------------------------------------------------------------
-    data_c_DOWN=np.array([-1.,1.])
-    x_c_DOWN=np.array([2,4])
-    y_c_DOWN=np.array([1,3])
-    c_DOWN=csr_matrix((data_c_DOWN,(x_c_DOWN-1,y_c_DOWN-1)),shape=(4,4))
-    # ---------------------------------------------------------------
-    data_n_UP=np.array([1.,1.])
-    x_n_UP=np.array([1,2])
-    y_n_UP=np.array([1,2])
-    n_UP=csr_matrix((data_n_UP,(x_n_UP-1,y_n_UP-1)),shape=(4,4))
-    # ---------------------------------------------------------------
-    data_n_DOWN=np.array([1.,1.])
-    x_n_DOWN=np.array([1,3])
-    y_n_DOWN=np.array([1,3])
-    n_DOWN=csr_matrix((data_n_DOWN,(x_n_DOWN-1,y_n_DOWN-1)),shape=(4,4))
-    # ---------------------------------------------------------------
-    data_n_PAIR=np.array([1.])
-    x_n_PAIR=np.array([1])
-    y_n_PAIR=np.array([1])
-    n_PAIR=csr_matrix((data_n_PAIR,(x_n_PAIR-1,y_n_PAIR-1)),shape=(4,4))
-    # ---------------------------------------------------------------
-    data_JW=np.array([1.,-1.,-1.,1.])
-    x_JW=np.array([1,2,3,4])
-    y_JW=np.array([1,2,3,4])
-    JW=csr_matrix((data_JW,(x_JW-1,y_JW-1)),shape=(4,4))
-    # ---------------------------------------------------------------
-    ID=identity(4)
-    return c_UP, c_DOWN, n_UP, n_DOWN, n_PAIR, JW, ID
+__all__ = [
+    "fermi_operators",
+    "bose_operators",
+]
 
 
-def get_bose_hubbard_operators(n_max):
-    entries=np.arange(1,n_max+1,1)
-    entries=np.sqrt(entries)
-    x_coords=np.arange(0,n_max,1)
-    y_coords=np.arange(1,n_max+1,1)
-    b_dagger_Op=csr_matrix((entries,(x_coords,y_coords)),shape=(n_max+1,n_max+1))
-    b_Op=csr_matrix(b_dagger_Op.conj().transpose())
-    num_Op=b_dagger_Op*b_Op
-    ID=identity(n_max+1)
+def fermi_operators(has_spin):
+    """
+    This functions define the matter field operators of the QED Hamiltonian.
+    They are related to spinless Dirac Fermions occupying lattice sites
+
+    Returns:
+        dict: dictionary with single site matter field operators
+    """
+    ops = {}
+    # Define the MATTER FIELDS OPERATORS
+    ops["psi"] = diags(np.array([1], dtype=float), 1, (2, 2))
+    ops["psi_dag"] = ops["psi"].transpose()
+    ops["P_psi"] = diags(np.array([1, -1], dtype=float), 0, (2, 2))
+    ops["N"] = ops["psi_dag"] * ops["psi"]
+    ops["ID_psi"] = identity(2)
+    if has_spin:
+        # up & down MATTER OPERATORS
+        ops["psi_up"] = qmb_op(ops, ["psi", "ID"])
+        ops["psi_down"] = qmb_op(ops, ["P_psi", "psi"])
+        ops["N_up"] = qmb_op(ops, ["N", "ID"])
+        ops["N_down"] = qmb_op(ops, ["ID", "N"])
+        # other number operators
+        ops["N_pair"] = ops["N_up"] * ops["N_down"]
+        ops["N_tot"] = ops["N_up"] + ops["N_down"]
+        ops["N_single"] = ops["N_tot"] - 2 * ops["N_pair"]
+        # identity on the whole matter site
+        ops["ID_psi"] = identity(4, dtype=float)
+        for s in ["up", "down"]:
+            ops[f"psi_{s}_dag"] = ops[f"psi_{s}"].transpose()
+    return ops
+
+
+def bose_operators(n_max):
+    entries = np.arange(1, n_max + 1, 1)
+    entries = np.sqrt(entries)
+    x_coords = np.arange(0, n_max, 1)
+    y_coords = np.arange(1, n_max + 1, 1)
+    b_dagger_Op = csr_matrix(
+        (entries, (x_coords, y_coords)), shape=(n_max + 1, n_max + 1)
+    )
+    b_Op = csr_matrix(b_dagger_Op.conj().transpose())
+    num_Op = b_dagger_Op * b_Op
+    ID = identity(n_max + 1)
     return b_dagger_Op, b_Op, num_Op, ID
-    
