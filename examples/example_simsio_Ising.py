368ef61960f4f9f7009ea52c4910b5f7064feb4e
Giovanni Cataldi <giovacataldi96@gmail.com>
2024-01-13 00:41:00 +0100

    update simsio version of Ising removing M and N matrices

--- examples/example_simsio_Ising.py
+++ examples/example_simsio_Ising.py
@@ -2,9 +2,7 @@ import numpy as np
 from math import prod
 from scipy.linalg import eigh
 from ed_lgt.operators import get_spin_operators
-from ed_lgt.modeling import Ground_State, LocalTerm, TwoBodyTerm
-from ed_lgt.modeling import get_state_configurations, truncation
-from ed_lgt.tools import check_hermitian
+from ed_lgt.modeling import LocalTerm, TwoBodyTerm, QMB_hamiltonian
 from simsio import run_sim
 
 with run_sim() as sim:
@@ -25,33 +23,31 @@ with run_sim() as sim:
     # ACQUIRE HAMILTONIAN COEFFICIENTS
     coeffs = {"J": sim.par["J"], "h": sim.par["h"]}
     # CONSTRUCT THE HAMILTONIAN
-    H = 0
+    H = QMB_hamiltonian(0, lvals, loc_dims)
     h_terms = {}
     # ---------------------------------------------------------------------------
-    # NEAsim.REST NEIGHBOR INTERACTION
+    # NEAREST NEIGHBOR INTERACTION
     for d in directions:
-        op_name_list = ["Sx", "Sx"]
-        op_list = [ops[op] for op in op_name_list]
+        op_names_list = ["Sx", "Sx"]
+        op_list = [ops[op] for op in op_names_list]
         # Define the Hamiltonian term
         h_terms[f"NN_{d}"] = TwoBodyTerm(
             axis=d,
             op_list=op_list,
-            op_name_list=op_name_list,
+            op_names_list=op_names_list,
             lvals=lvals,
             has_obc=has_obc,
         )
-        H += h_terms[f"NN_{d}"].get_Hamiltonian(strength=coeffs["J"])
+        H.Ham += h_terms[f"NN_{d}"].get_Hamiltonian(strength=coeffs["J"])
     # EXTERNAL MAGNETIC FIELD
     op_name = "Sz"
     h_terms[op_name] = LocalTerm(ops[op_name], op_name, lvals=lvals, has_obc=has_obc)
-    H += h_terms[op_name].get_Hamiltonian(strength=coeffs["h"])
+    H.Ham += h_terms[op_name].get_Hamiltonian(strength=coeffs["h"])
     # ===========================================================================
-    # CHECK THAT THE HAMILTONIAN IS HERMITIAN
-    check_hermitian(H)
     # DIAGONALIZE THE HAMILTONIAN
-    GS = Ground_State(H, n_eigs)
-    # Dictionary for results
-    sim.res["energy"] = GS.Nenergies
+    H.diagonalize(n_eigs)
+    sim.res["energy"] = H.Nenergies
+    sim.res["DeltaE"] = []
     # ===========================================================================
     # LIST OF LOCAL OBSERVABLES
     loc_obs = ["Sx", "Sz"]
@@ -65,7 +61,7 @@ with run_sim() as sim:
         h_terms[f"{obs1}_{obs2}"] = TwoBodyTerm(
             axis="x",
             op_list=op_list,
-            op_name_list=[obs1, obs2],
+            op_names_list=[obs1, obs2],
             lvals=lvals,
             has_obc=has_obc,
         )
@@ -73,83 +69,18 @@ with run_sim() as sim:
     for ii in range(n_eigs):
         print("====================================================")
         print(f"{ii} ENERGY: {format(sim.res['energy'][ii], '.9f')}")
+        if ii > 0:
+            sim.res["DeltaE"].append(sim.res["energy"][ii] - sim.res["energy"][0])
         # GET STATE CONFIGURATIONS
-        get_state_configurations(truncation(GS.Npsi[:, ii], 1e-6), loc_dims, lvals)
+        H.Npsi[ii].get_state_configurations(threshold=1e-3)
         # =======================================================================
         # MEASURE LOCAL OBSERVABLES:
         for obs in loc_obs:
-            h_terms[obs].get_expval(GS.Npsi[:, ii])
+            h_terms[obs].get_expval(H.Npsi[ii])
             sim.res[obs].append(h_terms[obs].avg)
         # MEASURE TWOBODY OBSERVABLES:
         for obs1, obs2 in twobody_obs:
             print("----------------------------------------------------")
             print(f"{obs1}_{obs2}")
             print("----------------------------------------------------")
-            h_terms[f"{obs1}_{obs2}"].get_expval(GS.Npsi[:, ii])
-            print(h_terms[f"{obs1}_{obs2}"].corr)
-    # %%
-    print(GS.Nenergies)
-    # %%
-    # Compute the Matrices for the generalized Eigenvalue Problem: Mx = w Nx
-    M = np.zeros((n_sites, n_sites), dtype=complex)
-    N = np.zeros((n_sites, n_sites), dtype=complex)
-
-    for ii in range(n_sites):
-        for jj in range(n_sites):
-            nn_condition = [
-                (ii > 0) and (jj == ii - 1),
-                (ii < n_sites) and (jj == ii + 1),
-                np.all([not has_obc, ii == 0, jj == n_sites - 1]),
-                np.all([not has_obc, ii == n_sites - 1, jj == 0]),
-            ]
-            if np.any(nn_condition):
-                print(ii, jj)
-                M[ii, jj] += coeffs["J"] * h_terms["Sz_Sz"].corr[ii, jj]
-            elif jj == ii:
-                # ---------------------------------------------------
-                N[ii, jj] = h_terms["Sz"].obs[ii]
-                # ---------------------------------------------------
-                M[ii, jj] += coeffs["h"] * h_terms["Sz"].obs[ii]
-                if ii < n_sites - 1:
-                    M[ii, jj] += (
-                        0.5
-                        * coeffs["J"]
-                        * (
-                            h_terms["Sm_Sx"].corr[ii, ii + 1]
-                            - h_terms["Sp_Sx"].corr[ii, ii + 1]
-                        )
-                    )
-                else:
-                    if not has_obc:
-                        M[ii, jj] += (
-                            0.5
-                            * coeffs["J"]
-                            * (
-                                h_terms["Sm_Sx"].corr[ii, 0]
-                                - h_terms["Sp_Sx"].corr[ii, 0]
-                            )
-                        )
-                if ii > 0:
-                    M[ii, jj] += (
-                        0.5
-                        * coeffs["J"]
-                        * (
-                            h_terms["Sx_Sm"].corr[ii - 1, ii]
-                            - h_terms["Sx_Sp"].corr[ii - 1, ii]
-                        )
-                    )
-                else:
-                    M[ii, jj] += (
-                        0.5
-                        * coeffs["J"]
-                        * (
-                            h_terms["Sx_Sm"].corr[n_sites - 1, ii]
-                            - h_terms["Sx_Sp"].corr[n_sites - 1, ii]
-                        )
-                    )
-
-    # %%
-    # Solve the generalized egenvalue problem
-    w = eigh(a=M, b=N, eigvals_only=True)
-
-    # %%
+            h_terms[f"{obs1}_{obs2}"].get_expval(H.Npsi[ii])
