f8799d0f5e6be3295919e2ffed762f7f99ac745c
Giovanni Cataldi <giovacataldi96@gmail.com>
2024-01-14 23:15:53 +0100

    replace all[] with all([])

--- ed_lgt/operators/ising_gaps.py
+++ ed_lgt/operators/ising_gaps.py
@@ -18,8 +18,8 @@ def get_M_operator(lvals, ht, coeffs, has_obc):
     M = np.zeros((n, n), dtype=complex)
     for ii, jj in product(range(n), repeat=2):
         nn_condition = [
-            (ii > 0) and (jj == ii - 1),
-            (ii < n - 1) and (jj == ii + 1),
+            all([ii > 0, jj == ii - 1]),
+            all([ii < n - 1, jj == ii + 1]),
             all([not has_obc, ii == 0, jj == n - 1]),
             all([not has_obc, ii == n - 1, jj == 0]),
         ]
@@ -27,7 +27,7 @@ def get_M_operator(lvals, ht, coeffs, has_obc):
             M[ii, jj] += coeffs["J"] * ht["Sz_Sz"].corr[ii, jj]
         elif jj == ii:
             M[ii, jj] += 2 * coeffs["h"] * ht["Sz"].obs[ii]
-            if 0 < ii < n - 1 or all[(ii == 0 or ii == n - 1), not has_obc]:
+            if 0 < ii < n - 1 or all([(ii == 0 or ii == n - 1), not has_obc]):
                 M[ii, jj] += complex(0, 0.5 * coeffs["J"]) * (
                     ht["Sm_Sx"].corr[ii, (ii + 1) % n]
                     - ht["Sp_Sx"].corr[ii, (ii + 1) % n]
@@ -49,9 +49,9 @@ def get_Q_operator(lvals, ht, has_obc, degree=2):
             # ---------------------- CASE Q_01 ----------------------------------------
             elif alpha == 0 and beta == 1:
                 for ii, jj in product(range(n), repeat=2):
-                    if jj == ii and any[ii < n - 1, all[ii == n - 1, not has_obc]]:
+                    if jj == ii and any([ii < n - 1, all([ii == n - 1, not has_obc])]):
                         Q[alpha, beta][ii, jj] += ht["Sm_Sz"].corr[ii, (jj + 1) % n]
-                    if jj == ii - 1 and any[ii > 0, all[ii == 0, not has_obc]]:
+                    if jj == ii - 1 and any([ii > 0, all([ii == 0, not has_obc])]):
                         Q[alpha, beta][ii, jj] += ht["Sm_Sz"].corr[jj % n, ii]
             # ---------------------- CASE Q_10 = Q_01 dag -----------------------------
             elif alpha == 1 and beta == 0:
@@ -59,18 +59,20 @@ def get_Q_operator(lvals, ht, has_obc, degree=2):
             # ---------------------- CASE Q_11 ----------------------------------------
             elif alpha == 1 and beta == 1:
                 for ii, jj in product(range(n), repeat=2):
-                    if (
-                        jj == ii - 1
-                        and any[
-                            0 < ii < n - 1, all[any[ii == 0, ii == n - 1], not has_obc]
+                    if jj == ii - 1 and any(
+                        [
+                            0 < ii < n - 1,
+                            all([any([ii == 0, ii == n - 1]), not has_obc]),
                         ]
                     ):
                         Q[alpha, beta][ii, jj] += ht["Sm_Sz_Sp"].corr[
                             jj % n, ii, (ii + 1) % n
                         ]
-                    if jj == ii and any[ii < n - 1, all[ii == n - 1, not has_obc]]:
+                    if jj == ii and any([ii < n - 1, all([ii == n - 1, not has_obc])]):
                         Q[alpha, beta][ii, jj] += ht["Sz_Sz"].corr[ii, (ii + 1) % n]
-                    if jj == ii + 1 and any[ii < n - 2, all[ii >= n - 2, not has_obc]]:
+                    if jj == ii + 1 and any(
+                        [ii < n - 2, all([ii >= n - 2, not has_obc])]
+                    ):
                         Q[alpha, beta][ii, jj] += ht["Sp_Sz_Sz"].corr[
                             ii, jj % n, (jj + 1) % n
                         ]
@@ -87,10 +89,10 @@ def get_P_operator(lvals, has_obc, ht, coeffs, degree=2):
             if alpha == beta == 0:
                 P[alpha, beta] += get_M_operator(lvals, ht, coeffs, has_obc)
             # ---------------------- CASE P_01 = P_10 ---------------------------------
-            elif any[all[alpha == 0, beta == 1], all[alpha == 1, beta == 0]]:
+            elif any([all([alpha == 0, beta == 1]), all([alpha == 1, beta == 0])]):
                 for ii, jj in product(range(n), repeat=2):
                     # -------------------------------------------------------------
-                    if ii == jj and any[ii < n - 1, ii == n - 1 and not has_obc]:
+                    if ii == jj and any([ii < n - 1, ii == n - 1 and not has_obc]):
                         P[alpha, beta][ii, jj] += (
                             complex(0, 2 * coeffs["J"])
                             * (
@@ -108,11 +110,10 @@ def get_P_operator(lvals, has_obc, ht, coeffs, degree=2):
                                 + ht["Sz_Sm"].corr[jj, (jj + 1) % n]
                             )
                         )
-                    if (
-                        ii == jj
-                        and any[
+                    if ii == jj and any(
+                        [
                             0 < ii < n - 1,
-                            all[(0 == ii or ii == n - 1), not has_obc],
+                            all([(0 == ii or ii == n - 1), not has_obc]),
                         ]
                     ):
                         P[alpha, beta][ii, jj] += complex(0, 1) * (
@@ -120,22 +121,21 @@ def get_P_operator(lvals, has_obc, ht, coeffs, degree=2):
                             - ht["Sx_Sm_Sm"].corr[(jj - 1) % n, ii, (jj + 1) % n]
                         )
                     # -------------------------------------------------------------
-                    if jj == ii + 1 and any[ii < n - 2, ii >= n - 2 and not has_obc]:
+                    if jj == ii + 1 and any([ii < n - 2, ii >= n - 2 and not has_obc]):
                         P[alpha, beta][ii, jj] += (
                             ht["Sz_Sp_Sp"].corr[ii, jj % n, (jj + 1) % n]
                             - ht["Sz_Sm_Sm"].corr[ii, jj % n, (jj + 1) % n]
                             - coeffs["J"] * ht["Sz_Sz"].corr[ii, jj]
                         )
                     # -------------------------------------------------------------
-                    if jj == ii - 2 and any[ii > 1, ii <= 1 and not has_obc]:
+                    if jj == ii - 2 and any([ii > 1, ii <= 1 and not has_obc]):
                         P[alpha, beta][ii, jj] += -complex(coeffs["J"] * 0.5, 0) * (
                             ht["Sp_Sz_Sz"].corr[jj % n, (jj + 1) % n, ii]
                             + ht["Sm_Sz_Sz"].corr[jj % n, (jj + 1) % n, ii]
                         )
                     # -------------------------------------------------------------
-                    elif (
-                        jj == ii - 1
-                        and any[
+                    elif jj == ii - 1 and any(
+                        [
                             0 < ii < n - 1,
                             (0 == ii or ii == n - 1) and not has_obc,
                         ]
@@ -152,9 +152,8 @@ def get_P_operator(lvals, has_obc, ht, coeffs, degree=2):
             # ---------------------- CASE P_11 ----------------------------------------
             elif alpha == beta == 1:
                 for ii, jj in product(range(n), repeat=2):
-                    if (
-                        jj == ii - 2
-                        and any[
+                    if jj == ii - 2 and any(
+                        [
                             1 < ii < n - 1,
                             (1 >= ii or ii == n - 1) and not has_obc,
                         ]
@@ -167,9 +166,8 @@ def get_P_operator(lvals, has_obc, ht, coeffs, degree=2):
                                 jj % n, (jj + 1) % n, ii, (ii + 1) % n
                             ]
                         )
-                    if (
-                        jj == ii - 1
-                        and any[
+                    if jj == ii - 1 and any(
+                        [
                             0 < ii < n - 1,
                             (0 == ii or ii == n - 1) and not has_obc,
                         ]
@@ -192,9 +190,8 @@ def get_P_operator(lvals, has_obc, ht, coeffs, degree=2):
                             ht["Sp_Sz_Sm"].corr[jj % n, ii, (ii + 1) % n]
                             + ht["Sm_Sz_Sp"].corr[jj % n, ii, (ii + 1) % n]
                         )
-                    if (
-                        jj == ii
-                        and any[
+                    if jj == ii and any(
+                        [
                             0 < ii < n - 1,
                             (0 == ii or ii == n - 1) and not has_obc,
                         ]
@@ -203,7 +200,7 @@ def get_P_operator(lvals, has_obc, ht, coeffs, degree=2):
                             ht["Sx_Sp_Sz"].corr[(ii - 1) % n, ii, (ii + 1) % n]
                             - ht["Sx_Sm_Sz"].corr[(ii - 1) % n, ii, (ii + 1) % n]
                         )
-                    if jj == ii and any[ii < n - 1, ii == n - 1 and not has_obc]:
+                    if jj == ii and any([ii < n - 1, ii == n - 1 and not has_obc]):
                         P[alpha, beta][ii, jj] += (
                             complex(0, coeffs["J"])
                             * (
@@ -214,7 +211,7 @@ def get_P_operator(lvals, has_obc, ht, coeffs, degree=2):
                             )
                             + 4 * coeffs["h"] * ht["Sz_Sz"].corr[ii, (ii + 1) % n]
                         )
-                    if jj == ii + 1 and any[ii < n - 1, ii == n - 1 and not has_obc]:
+                    if jj == ii + 1 and any([ii < n - 1, ii == n - 1 and not has_obc]):
                         P[alpha, beta][ii, jj] += (
                             complex(0, 0.5 * coeffs["J"])
                             * (
@@ -241,7 +238,7 @@ def get_P_operator(lvals, has_obc, ht, coeffs, degree=2):
                                 + ht["Sm_Sz_Sp"].corr[ii, jj % n, (jj + 1) % n]
                             )
                         )
-                    if jj == ii + 2 and any[ii < n - 2, ii >= n - 2 and not has_obc]:
+                    if jj == ii + 2 and any([ii < n - 2, ii >= n - 2 and not has_obc]):
                         P[alpha, beta][ii, jj] += -(0.5 * coeffs["J"]) * (
                             ht["Sp_Sz_Sz_Sm"].corr[
                                 ii, (ii + 1) % n, jj % n, (jj + 1) % n
