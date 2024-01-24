da9f93426e9b8357a1fcf4a88fa223309d9bba7b
Giovanni Cataldi <giovacataldi96@gmail.com>
2024-01-24 23:32:03 +0100

    remove unused function. Optimize two functions: one for Parity sectors (Zn symmetries) one for U(1) symmetry sectors

--- ed_lgt/modeling/symmetries.py
+++ ed_lgt/modeling/symmetries.py
@@ -1,53 +1,41 @@
-# %%
 import numpy as np
+from math import prod
 from itertools import product
-from scipy.sparse import isspmatrix, lil_matrix
+
+__all__ = ["U_sector_indices", "P_sector_indices"]
 
 
-def sector_indices(loc_dims, op_list, sectors_list):
+def P_sector_indices(loc_dims, op_list, sectors_list):
+    # For the moment it works only with same loc_dim
     basis = []
-    for n, config in enumerate(list(product(*[range(dim) for dim in loc_dims]))):
-        include_state = True
-        for op, sector_value in zip(op_list, sectors_list):
-            # Calculate the value of the operator in the configuration config
-            tmp = sum(op[c, c] for c in config)
-            # Check if the state belongs to the sector
-            if tmp != sector_value:
-                include_state = False
-                break
-        if include_state:
-            # Save the configuration state in the basis projector
+    config_basis = []
+    # Precompute the ranges for each dimension
+    ranges = [range(dim) for dim in loc_dims]
+    # Precompute the diagonals of the operators
+    op_diagonals = [np.diag(op) for op in op_list]
+    for n, config in enumerate(list(product(*ranges))):
+        if all(
+            prod(op_diag[c] for c in config) == sector
+            for op_diag, sector in zip(op_diagonals, sectors_list)
+        ):
             basis.append(n)
-            print(n, config)
-    print(len(basis))
-    return basis
+            config_basis.append(list(config))
+    return basis, config_basis
 
 
-def build_H_block(H, loc_dims, site_basis, op_list, sectors_list):
-    basis = sector_indices(loc_dims, site_basis, op_list, sectors_list)
-    H_sector = H[basis, :][:, basis]
-    return H_sector
-
-
-def get_submatrix_from_sparse(matrix, rows_list, cols_list):
-    if not isspmatrix(matrix):
-        raise TypeError(f"matrix must be a SPARSE MATRIX, not a {type(matrix)}")
-    if not isinstance(rows_list, list):
-        raise TypeError(f"rows_list must be a LIST, not a {type(rows_list)}")
-    if not isinstance(cols_list, list):
-        raise TypeError(f"cols_list must be a LIST, not a {type(cols_list)}")
-    matrix = lil_matrix(matrix)
-    # Get the Submatrix out of the list of rows and columns
-    sub_matrix = matrix[rows_list, :][:, cols_list]
-    return sub_matrix
-
-
-"""
-loc_dims = [4, 4, 4, 4]
-op1 = np.diag([0, 1, 1, 2])
-op2 = np.diag([0, 1, 0, 1])
-sectors = [6, 3]
-op_list = [op1, op2]
-basis = sector_indices(loc_dims, op_list, sectors)
-"""
-# %%
+def U_sector_indices(loc_dims, op_list, sectors_list):
+    # For the moment it works only with same loc_dim
+    basis = []
+    config_basis = []
+    # Precompute the ranges for each dimension
+    ranges = [range(dim) for dim in loc_dims]
+    # Precompute the diagonals of the operators
+    op_diagonals = [np.diag(op) for op in op_list]
+    for n, config in enumerate(list(product(*ranges))):
+        if all(
+            sum(op_diag[c] for c in config) == sector
+            for op_diag, sector in zip(op_diagonals, sectors_list)
+        ):
+            basis.append(n)
+            config_basis.append(list(config))
+    return basis, config_basis
