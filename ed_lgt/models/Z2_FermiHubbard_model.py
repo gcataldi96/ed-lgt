eaebe777b3a6eda8526e68b217401719a66543af
Giovanni Cataldi <giovacataldi96@gmail.com>
2024-01-26 17:58:54 +0100

    adjust model and script of Z2FermiHubbard to quantum Model

--- ed_lgt/models/Z2_FermiHubbard_model.py
+++ ed_lgt/models/Z2_FermiHubbard_model.py
@@ -4,37 +4,41 @@ from ed_lgt.operators import (
     Z2_FermiHubbard_dressed_site_operators,
     Z2_FermiHubbard_gauge_invariant_states,
 )
-from ed_lgt.modeling import LocalTerm, TwoBodyTerm, PlaquetteTerm, QMB_hamiltonian
-from ed_lgt.modeling import (
-    check_link_symmetry,
-    lattice_base_configs,
-)
+from ed_lgt.modeling import LocalTerm, TwoBodyTerm, QMB_hamiltonian
+from ed_lgt.modeling import check_link_symmetry, lattice_base_configs
+from .quantum_model import QuantumModel
 
 
 __all__ = ["Z2_FermiHubbard_Model"]
 
 
-class Z2_FermiHubbard_Model:
+class Z2_FermiHubbard_Model(QuantumModel):
     def __init__(self, params):
-        self.lvals = params["lvals"]
-        self.dim = len(self.lvals)
-        self.directions = "xyz"[: self.dim]
-        self.n_sites = prod(self.lvals)
-        self.has_obc = params["has_obc"]
-        self.coeffs = params["coeffs"]
-        self.n_eigs = params["n_eigs"]
-        self.sector = params["sector"]
+        # Initialize base class with the common parameters
+        super().__init__(params)
+        # Initialize specific attributes for Z2 FermiHubbardModel
+        self.sector = params["sym_sector"]
+        self.sector_basis = None
+        self.sector_indices = None
+
+    def get_operators(self, sparse=True):
         # ACQUIRE OPERATORS AS CSR MATRICES IN A DICTIONARY
         self.ops = Z2_FermiHubbard_dressed_site_operators(lattice_dim=self.dim)
+        if not sparse:
+            for op in self.ops.keys():
+                self.ops[op] = self.ops[op].toarray()
+
+    def get_local_site_dimensions(self):
         # ACQUIRE BASIS AND GAUGE INVARIANT SITES
-        self.basis, _ = Z2_FermiHubbard_gauge_invariant_states(lattice_dim=self.dim)
+        self.site_basis, _ = Z2_FermiHubbard_gauge_invariant_states(
+            lattice_dim=self.dim
+        )
         # ACQUIRE LOCAL DIMENSION OF EVERY SINGLE SITE
         lattice_base, loc_dims = lattice_base_configs(
-            self.basis, self.lvals, self.has_obc
+            self.site_basis, self.lvals, self.has_obc
         )
         self.loc_dims = loc_dims.transpose().reshape(self.n_sites)
         self.lattice_base = lattice_base.transpose().reshape(self.n_sites)
-        self.res = {}
 
     def build_Hamiltonian(self):
         # CONSTRUCT THE HAMILTONIAN
@@ -52,7 +56,9 @@ class Z2_FermiHubbard_Model:
                 op_names_list=op_names_list,
                 lvals=self.lvals,
                 has_obc=self.has_obc,
-                site_basis=self.basis,
+                site_basis=self.site_basis,
+                sector_indices=self.sector_indices,
+                sector_basis=self.sector_basis,
             )
             self.H.Ham += h_terms[f"W{d}"].get_Hamiltonian(
                 strength=-2 * self.coeffs["eta"]
@@ -64,7 +70,9 @@ class Z2_FermiHubbard_Model:
             op_name,
             lvals=self.lvals,
             has_obc=self.has_obc,
-            site_basis=self.basis,
+            site_basis=self.site_basis,
+            sector_indices=self.sector_indices,
+            sector_basis=self.sector_basis,
         )
         self.H.Ham += h_terms[op_name].get_Hamiltonian(strength=self.coeffs["eta"])
         # -------------------------------------------------------------------------------
@@ -75,7 +83,9 @@ class Z2_FermiHubbard_Model:
             op_name,
             lvals=self.lvals,
             has_obc=self.has_obc,
-            site_basis=self.basis,
+            site_basis=self.site_basis,
+            sector_indices=self.sector_indices,
+            sector_basis=self.sector_basis,
         )
         self.H.Ham += h_terms["V"].get_Hamiltonian(strength=self.coeffs["U"])
         # -------------------------------------------------------------------------------
@@ -92,7 +102,9 @@ class Z2_FermiHubbard_Model:
                     op_names_list,
                     lvals=self.lvals,
                     has_obc=self.has_obc,
-                    site_basis=self.basis,
+                    site_basis=self.site_basis,
+                    sector_indices=self.sector_indices,
+                    sector_basis=self.sector_basis,
                 )
                 self.H.Ham += h_terms[f"{d}_hop_{s}"].get_Hamiltonian(
                     strength=self.coeffs["t"], add_dagger=True
@@ -106,7 +118,7 @@ class Z2_FermiHubbard_Model:
                 op_name,
                 lvals=self.lvals,
                 has_obc=self.has_obc,
-                site_basis=self.basis,
+                site_basis=self.site_basis,
             )
             self.H.Ham += (
                 0.5
@@ -117,62 +129,6 @@ class Z2_FermiHubbard_Model:
                 )
                 ** 2
             )
-        # DIAGONALIZE THE HAMILTONIAN
-        self.H.diagonalize(self.n_eigs)
-        self.res["energies"] = self.H.Nenergies
-        if self.n_eigs > 1:
-            self.res["true_gap"] = self.H.Nenergies[1] - self.H.Nenergies[0]
-
-    def get_observables(self, local_obs, twobody_obs, plaquette_obs):
-        self.local_obs = local_obs
-        self.twobody_obs = twobody_obs
-        self.plaquette_obs = plaquette_obs
-        self.obs_list = {}
-        # ---------------------------------------------------------------------------
-        # LIST OF LOCAL OBSERVABLES
-        for obs in local_obs:
-            self.obs_list[obs] = LocalTerm(
-                self.ops[obs],
-                obs,
-                lvals=self.lvals,
-                has_obc=self.has_obc,
-                site_basis=self.basis,
-            )
-        # ---------------------------------------------------------------------------
-        # LIST OF TWOBODY CORRELATORS
-        for op_names_list in twobody_obs:
-            obs = "_".join(op_names_list)
-            op_list = [self.ops[op] for op in op_names_list]
-            self.obs_list[obs] = TwoBodyTerm(
-                axis="x",
-                op_list=op_list,
-                op_names_list=op_names_list,
-                lvals=self.lvals,
-                has_obc=self.has_obc,
-                site_basis=self.basis,
-            )
-        # ---------------------------------------------------------------------------
-        # LIST OF PLAQUETTE CORRELATORS
-        for op_names_list in plaquette_obs:
-            obs = "_".join(op_names_list)
-            op_list = [self.ops[op] for op in op_names_list]
-            self.obs_list[obs] = PlaquetteTerm(
-                axis=["x", "y"],
-                op_list=op_list,
-                op_names_list=op_names_list,
-                lvals=self.lvals,
-                has_obc=self.has_obc,
-                site_basis=self.basis,
-            )
-
-    def measure_observables(self, state_number):
-        for obs in self.local_obs:
-            self.obs_list[obs].get_expval(self.H.Npsi[state_number])
-            self.res[obs] = self.obs_list[obs].obs
-        for op_names_list in self.twobody_obs:
-            obs = "_".join(op_names_list)
-            self.obs_list[obs].get_expval(self.H.Npsi[state_number])
-            self.res[obs] = self.obs_list[obs].corr
 
     def check_symmetries(self):
         # CHECK LINK SYMMETRIES
--- scripts/script_Z2_FermiHubbard.py
+++ scripts/script_Z2_FermiHubbard.py
@@ -4,18 +4,29 @@ from simsio import run_sim
 with run_sim() as sim:
     sim.par["coeffs"]["U"] = sim.par["U"]
     model = Z2_FermiHubbard_Model(sim.par)
+    # GET LOCAL SITE DIMENSION
+    model.get_local_site_dimensions()
+    # GET SYMMETRY SECTOR
+    sym_sector = sim.par["sym_sector"]
+    model.sector = sym_sector
+    if sym_sector is not None:
+        # GET OPERATORS
+        model.get_operators(sparse=False)
+        model.get_abelian_symmetry_sector(["N_tot"], [sym_sector], sym_type="U")
+    else:
+        # GET OPERATORS
+        model.get_operators()
     # BUILD AND DIAGONALIZE HAMILTONIAN
     model.build_Hamiltonian()
+    model.diagonalize_Hamiltonian()
     # LIST OF LOCAL OBSERVABLES
     local_obs = [f"n_{s}{d}" for d in model.directions for s in "mp"]
     local_obs += [f"N_{label}" for label in ["up", "down", "tot", "single", "pair"]]
     local_obs += ["X_Cross"]
     # LIST OF TWOBODY CORRELATORS
     twobody_obs = [["P_px", "P_mx"], ["P_py", "P_my"]]
-    # LIST OF PLAQUETTE CORRELATORS
-    plaquette_obs = []  # ["C_px,py", "C_py,mx", "C_my,px", "C_mx,my"]
     # DEFINE OBSERVABLES
-    model.get_observables(local_obs, twobody_obs, plaquette_obs)
+    model.get_observables(local_obs, twobody_obs)
     # MEASUREMENTS
     sim.res["obs"] = {}
     for ii in range(model.n_eigs):
