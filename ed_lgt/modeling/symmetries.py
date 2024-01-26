7e6169741c2ec1ae5c6c9f6ca194e70d190117b9
Giovanni Cataldi <giovacataldi96@gmail.com>
2024-01-26 18:16:10 +0100

    add raise Value Error for sym_type != P, U. rename sector_indices and sector_basis to uniform variables names

--- ed_lgt/modeling/symmetries.py
+++ ed_lgt/modeling/symmetries.py
@@ -7,8 +7,8 @@ __all__ = ["abelian_sector_indices"]
 
 def abelian_sector_indices(loc_dims, op_list, op_sectors_list, sym_type="U"):
     # For the moment it works only with same loc_dim
-    basis = []
-    config_basis = []
+    sector_indices = []
+    sector_basis = []
     # Precompute the ranges for each dimension
     ranges = [range(dim) for dim in loc_dims]
     # Precompute the diagonals of the operators
@@ -24,7 +24,11 @@ def abelian_sector_indices(loc_dims, op_list, op_sectors_list, sym_type="U"):
                 prod(op_diag[c] for c in config) == sector
                 for op_diag, sector in zip(op_diagonals, op_sectors_list)
             )
+        else:
+            raise ValueError(
+                f"For now sym_type can only be P (parity) or U(1), not {sym_type}"
+            )
         if check:
-            basis.append(n)
-            config_basis.append(list(config))
-    return np.array(basis, dtype=int), np.array(config_basis, dtype=int)
+            sector_indices.append(n)
+            sector_basis.append(list(config))
+    return np.array(sector_indices, dtype=int), np.array(sector_basis, dtype=int)
