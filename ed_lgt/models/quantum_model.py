398be806782d6df038db455429ef6397ff383e08
Giovanni Cataldi <giovacataldi96@gmail.com>
2024-02-03 00:08:10 +0100

    add measurements of nbody operators

--- ed_lgt/models/quantum_model.py
+++ ed_lgt/models/quantum_model.py
@@ -1,5 +1,5 @@
 from math import prod
-from ed_lgt.modeling import LocalTerm, TwoBodyTerm, PlaquetteTerm
+from ed_lgt.modeling import LocalTerm, TwoBodyTerm, PlaquetteTerm, NBodyTerm
 from ed_lgt.modeling import QMB_hamiltonian
 from ed_lgt.modeling import abelian_sector_indices
 
@@ -43,10 +43,13 @@ class QuantumModel:
             if self.n_eigs > 1:
                 self.res["true_gap"] = self.H.Nenergies[1] - self.H.Nenergies[0]
 
-    def get_observables(self, local_obs=[], twobody_obs=[], plaquette_obs=[]):
+    def get_observables(
+        self, local_obs=[], twobody_obs=[], plaquette_obs=[], nbody_obs=[]
+    ):
         self.local_obs = local_obs
         self.twobody_obs = twobody_obs
         self.plaquette_obs = plaquette_obs
+        self.nbody_obs = nbody_obs
         self.obs_list = {}
         # ---------------------------------------------------------------------------
         # LIST OF LOCAL OBSERVABLES
@@ -88,12 +91,30 @@ class QuantumModel:
                 has_obc=self.has_obc,
                 site_basis=self.site_basis,
             )
+        # ---------------------------------------------------------------------------
+        # LIST OF NBODY CORRELATORS
+        for op_names_list in nbody_obs:
+            obs = "_".join(op_names_list)
+            op_list = [self.ops[op] for op in op_names_list]
+            self.obs_list[obs] = NBodyTerm(
+                op_list=op_list,
+                op_names_list=op_names_list,
+                lvals=self.lvals,
+                has_obc=self.has_obc,
+                site_basis=self.site_basis,
+                sector_indices=self.sector_indices,
+                sector_basis=self.sector_basis,
+            )
 
     def measure_observables(self, state_number):
         for obs in self.local_obs:
             self.obs_list[obs].get_expval(self.H.Npsi[state_number])
             self.res[obs] = self.obs_list[obs].obs
         for op_names_list in self.twobody_obs:
+            obs = "_".join(op_names_list)
+            self.obs_list[obs].get_expval(self.H.Npsi[state_number])
+            self.res[obs] = self.obs_list[obs].corr
+        for op_names_list in self.nbody_obs:
             obs = "_".join(op_names_list)
             self.obs_list[obs].get_expval(self.H.Npsi[state_number])
             self.res[obs] = self.obs_list[obs].corr
