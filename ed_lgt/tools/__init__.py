e8a09e7a06c878ad4a4e7fbbd1b3a387dafa1cd7
Giovanni Cataldi <giovacataldi96@gmail.com>
2024-02-14 21:23:55 +0100

    update tools init

--- ed_lgt/tools/__init__.py
+++ ed_lgt/tools/__init__.py
@@ -2,19 +2,16 @@ from . import (
     checks,
     derivatives,
     manage_data,
-    LGT_analysis,
     measures,
 )
 from .checks import *
 
 from .derivatives import *
 from .manage_data import *
-from .LGT_analysis import *
 from .measures import *
 
 # All modules have an __all__ defined
 __all__ = checks.__all__.copy()
 __all__ += derivatives.__all__.copy()
 __all__ += manage_data.__all__.copy()
-__all__ += LGT_analysis.__all__.copy()
 __all__ += measures.__all__.copy()
