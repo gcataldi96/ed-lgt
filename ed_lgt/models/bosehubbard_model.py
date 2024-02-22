b2af7db717d45a50baea091a7eb98b65284221a1
Giovanni Cataldi <giovacataldi96@gmail.com>
2024-02-22 18:55:18 +0100

    update bosehubbard with symmetries

--- ed_lgt/models/bosehubbard_model.py
+++ ed_lgt/models/bosehubbard_model.py
@@ -7,20 +7,21 @@ __all__ = ["BoseHubbard_Model"]
 
 
 class BoseHubbard_Model(QuantumModel):
-    def __init__(self, params):
+    def __init__(self, n_max, **kwargs):
         # Initialize base class with the common parameters
-        super().__init__(params)
+        super().__init__(**kwargs)
         # Initialize specific attributes for BOSE HUBBARD model
-        self.n_max = params["n_max"]
-        self.loc_dims = np.array([self.n_max + 1 for _ in range(self.n_sites)])
-
-    def get_operators(self, sparse=True):
+        self.n_max = n_max
+        self.loc_dims = np.array(
+            [self.n_max + 1 for _ in range(self.n_sites)], dtype=np.uint8
+        )
+        # Acquire operators
         self.ops = bose_operators(self.n_max)
-        if not sparse:
-            for op in self.ops.keys():
-                self.ops[op] = self.ops[op].toarray()
+        # Acquire lattice label
+        self.get_local_site_dimensions()
 
-    def build_Hamiltonian(self):
+    def build_Hamiltonian(self, coeffs):
+        self.coeffs = coeffs
         # CONSTRUCT THE HAMILTONIAN
         self.H = QMB_hamiltonian(0, self.lvals, self.loc_dims)
         h_terms = {}
@@ -31,13 +32,7 @@ class BoseHubbard_Model(QuantumModel):
             op_list = [self.ops[op] for op in op_names_list]
             # Define the Hamiltonian term
             h_terms[f"NN_{d}"] = TwoBodyTerm(
-                axis=d,
-                op_list=op_list,
-                op_names_list=op_names_list,
-                lvals=self.lvals,
-                has_obc=self.has_obc,
-                sector_basis=self.sector_basis,
-                sector_indices=self.sector_indices,
+                axis=d, op_list=op_list, op_names_list=op_names_list, **self.def_params
             )
             self.H.Ham += h_terms[f"NN_{d}"].get_Hamiltonian(
                 strength=-self.coeffs["t"], add_dagger=True
@@ -45,22 +40,12 @@ class BoseHubbard_Model(QuantumModel):
         # SINGLE SITE POTENTIAL
         op_name = "N2"
         h_terms[op_name] = LocalTerm(
-            operator=self.ops[op_name],
-            op_name=op_name,
-            lvals=self.lvals,
-            has_obc=self.has_obc,
-            sector_basis=self.sector_basis,
-            sector_indices=self.sector_indices,
+            operator=self.ops[op_name], op_name=op_name, **self.def_params
         )
         self.H.Ham += h_terms[op_name].get_Hamiltonian(strength=0.5 * self.coeffs["U"])
         op_name = "N"
         h_terms[op_name] = LocalTerm(
-            operator=self.ops[op_name],
-            op_name=op_name,
-            lvals=self.lvals,
-            has_obc=self.has_obc,
-            sector_basis=self.sector_basis,
-            sector_indices=self.sector_indices,
+            operator=self.ops[op_name], op_name=op_name, **self.def_params
         )
         self.H.Ham += h_terms[op_name].get_Hamiltonian(strength=-0.5 * self.coeffs["U"])
         # ADD SINGLE SITE NOISE
--- scripts/script_bosehubbard.py
+++ scripts/script_bosehubbard.py
@@ -2,20 +2,22 @@ from ed_lgt.models import BoseHubbard_Model
 from simsio import run_sim
 
 with run_sim() as sim:
-    sim.par["coeffs"] = {"t": sim.par["t"], "U": sim.par["U"]}
-    model = BoseHubbard_Model(sim.par)
-    # GET SYMMETRY SECTOR
-    sym_sector = sim.par["sym_sector"]
-    if sym_sector is not None:
-        # GET OPERATORS
-        model.get_operators(sparse=False)
-        model.get_abelian_symmetry_sector(["N"], [sym_sector], sym_type="U")
-    else:
-        # GET OPERATORS
-        model.get_operators()
+    model = BoseHubbard_Model(**sim.par["model"])
+    # SYMMETRIES
+    global_ops = [model.ops[op] for op in sim.par["symmetries"]["sym_ops"]]
+    global_sectors = sim.par["symmetries"]["sym_sectors"]
+    global_sym_type = sim.par["symmetries"]["sym_type"]
+    model.get_abelian_symmetry_sector(
+        global_ops=global_ops,
+        global_sectors=global_sectors,
+        global_sym_type=global_sym_type,
+    )
+    # DEFAUL PARAMS
+    model.default_params()
     # BUILD AND DIAGONALIZE HAMILTONIAN
-    model.build_Hamiltonian()
-    model.diagonalize_Hamiltonian()
+    coeffs = {"t": sim.par["t"], "U": sim.par["U"]}
+    model.build_Hamiltonian(coeffs)
+    model.diagonalize_Hamiltonian(**sim.par["hamiltonian"])
     # LIST OF LOCAL OBSERVABLES
     local_obs = ["N"]
     # LIST OF TWOBODY CORRELATORS
