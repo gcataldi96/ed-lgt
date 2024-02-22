b7a3c49a738c3c985efe6eb02620b618fb5a9004
Giovanni Cataldi <giovacataldi96@gmail.com>
2024-02-22 18:51:46 +0100

    new version of quantum_model compatible with symmetries

--- ed_lgt/models/quantum_model.py
+++ ed_lgt/models/quantum_model.py
@@ -1,41 +1,111 @@
+import numpy as np
 from math import prod
-from ed_lgt.modeling import LocalTerm, TwoBodyTerm, PlaquetteTerm, NBodyTerm
-from ed_lgt.modeling import QMB_hamiltonian
-from ed_lgt.modeling import abelian_sector_indices
+from ed_lgt.modeling import (
+    LocalTerm,
+    TwoBodyTerm,
+    PlaquetteTerm,
+    NBodyTerm,
+    QMB_hamiltonian,
+)
+from ed_lgt.symmetries import (
+    get_symmetry_sector_generators,
+    link_abelian_sector,
+    global_abelian_sector,
+)
+from ed_lgt.modeling import get_lattice_link_site_pairs, lattice_base_configs
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 __all__ = ["QuantumModel"]
 
 
 class QuantumModel:
-    def __init__(self, params):
+    def __init__(self, lvals, has_obc):
         # Lattice parameters
-        self.lvals = params["lvals"]
+        self.lvals = lvals
         self.dim = len(self.lvals)
         self.directions = "xyz"[: self.dim]
         self.n_sites = prod(self.lvals)
-        self.has_obc = params["has_obc"]
-        # Hamiltonian parameters
-        self.n_eigs = params["n_eigs"]
-        self.coeffs = params["coeffs"]
+        self.has_obc = has_obc
         # Symmetry sector indices
-        self.sector_basis = None
+        self.sector_configs = None
         self.sector_indices = None
         # Site Basis
-        self.site_basis = None
+        self.gauge_basis = None
         # Staggered Basis
         self.staggered_basis = False
         # Dictionary for results
         self.res = {}
 
-    def get_abelian_symmetry_sector(self, op_names_list, op_sectors_list, sym_type):
-        self.sector_indices, self.sector_basis = abelian_sector_indices(
-            self.loc_dims,
-            [self.ops[op] for op in op_names_list],
-            op_sectors_list,
-            sym_type,
-        )
+    def default_params(self):
+        self.def_params = {
+            "lvals": self.lvals,
+            "has_obc": self.has_obc,
+            "gauge_basis": self.gauge_basis,
+            "sector_configs": self.sector_configs,
+            "staggered_basis": self.staggered_basis,
+        }
 
-    def diagonalize_Hamiltonian(self):
+    def get_local_site_dimensions(self):
+        if self.gauge_basis is not None:
+            # Acquire local dimension and lattice label
+            lattice_labels, loc_dims = lattice_base_configs(
+                self.gauge_basis, self.lvals, self.has_obc, self.staggered_basis
+            )
+            self.loc_dims = loc_dims.transpose().reshape(self.n_sites)
+            self.lattice_labels = lattice_labels.transpose().reshape(self.n_sites)
+        else:
+            self.lattice_labels = None
+        logger.info(f"local dimensions: {self.loc_dims}")
+
+    def get_abelian_symmetry_sector(
+        self,
+        global_ops=None,
+        global_sectors=None,
+        global_sym_type="U",
+        link_ops=None,
+        link_sectors=None,
+    ):
+        # ================================================================================
+        # GLOBAL ABELIAN SYMMETRIES
+        if global_ops is not None:
+            global_ops = get_symmetry_sector_generators(
+                global_ops,
+                loc_dims=self.loc_dims,
+                action="global",
+                gauge_basis=self.gauge_basis,
+                lattice_labels=self.lattice_labels,
+            )
+            self.sector_indices, self.sector_configs = global_abelian_sector(
+                loc_dims=self.loc_dims,
+                sym_op_diags=global_ops,
+                sym_sectors=np.array(global_sectors, dtype=float),
+                sym_type=global_sym_type,
+                configs=self.sector_configs,
+            )
+        # ================================================================================
+        # ABELIAN Z2 SYMMETRIES
+        if link_ops is not None:
+            link_ops = get_symmetry_sector_generators(
+                link_ops,
+                loc_dims=self.loc_dims,
+                action="link",
+                gauge_basis=self.gauge_basis,
+                lattice_labels=self.lattice_labels,
+            )
+            site_pairs = get_lattice_link_site_pairs(self.lvals, self.has_obc)
+            self.sector_indices, self.sector_configs = link_abelian_sector(
+                loc_dims=self.loc_dims,
+                sym_op_diags=link_ops,
+                sym_sectors=link_sectors,
+                pair_list=site_pairs,
+                configs=self.sector_configs,
+            )
+
+    def diagonalize_Hamiltonian(self, n_eigs):
+        self.n_eigs = n_eigs
         if isinstance(self.H, QMB_hamiltonian):
             # DIAGONALIZE THE HAMILTONIAN
             self.H.diagonalize(self.n_eigs)
@@ -44,10 +114,16 @@ class QuantumModel:
                 self.res["true_gap"] = self.H.Nenergies[1] - self.H.Nenergies[0]
 
     def get_observables(
-        self, local_obs=[], twobody_obs=[], plaquette_obs=[], nbody_obs=[]
+        self,
+        local_obs=[],
+        twobody_obs=[],
+        plaquette_obs=[],
+        nbody_obs=[],
+        twobody_axes=None,
     ):
         self.local_obs = local_obs
         self.twobody_obs = twobody_obs
+        self.twobody_axes = twobody_axes
         self.plaquette_obs = plaquette_obs
         self.nbody_obs = nbody_obs
         self.obs_list = {}
@@ -55,28 +131,18 @@ class QuantumModel:
         # LIST OF LOCAL OBSERVABLES
         for obs in local_obs:
             self.obs_list[obs] = LocalTerm(
-                operator=self.ops[obs],
-                op_name=obs,
-                lvals=self.lvals,
-                has_obc=self.has_obc,
-                site_basis=self.site_basis,
-                sector_indices=self.sector_indices,
-                sector_basis=self.sector_basis,
+                operator=self.ops[obs], op_name=obs, **self.def_params
             )
         # ---------------------------------------------------------------------------
         # LIST OF TWOBODY CORRELATORS
-        for op_names_list in twobody_obs:
+        for ii, op_names_list in enumerate(twobody_obs):
             obs = "_".join(op_names_list)
             op_list = [self.ops[op] for op in op_names_list]
             self.obs_list[obs] = TwoBodyTerm(
-                axis="x",
+                axis=twobody_axes[ii] if twobody_axes is not None else "x",
                 op_list=op_list,
                 op_names_list=op_names_list,
-                lvals=self.lvals,
-                has_obc=self.has_obc,
-                site_basis=self.site_basis,
-                sector_indices=self.sector_indices,
-                sector_basis=self.sector_basis,
+                **self.def_params,
             )
         # ---------------------------------------------------------------------------
         # LIST OF PLAQUETTE CORRELATORS
@@ -87,9 +153,7 @@ class QuantumModel:
                 axes=["x", "y"],
                 op_list=op_list,
                 op_names_list=op_names_list,
-                lvals=self.lvals,
-                has_obc=self.has_obc,
-                site_basis=self.site_basis,
+                **self.def_params,
             )
         # ---------------------------------------------------------------------------
         # LIST OF NBODY CORRELATORS
@@ -97,13 +161,7 @@ class QuantumModel:
             obs = "_".join(op_names_list)
             op_list = [self.ops[op] for op in op_names_list]
             self.obs_list[obs] = NBodyTerm(
-                op_list=op_list,
-                op_names_list=op_names_list,
-                lvals=self.lvals,
-                has_obc=self.has_obc,
-                site_basis=self.site_basis,
-                sector_indices=self.sector_indices,
-                sector_basis=self.sector_basis,
+                op_list=op_list, op_names_list=op_names_list, **self.def_params
             )
 
     def measure_observables(self, state_number):
@@ -114,6 +172,12 @@ class QuantumModel:
             obs = "_".join(op_names_list)
             self.obs_list[obs].get_expval(self.H.Npsi[state_number])
             self.res[obs] = self.obs_list[obs].corr
+            if self.twobody_axes is not None:
+                self.obs_list[obs].print_nearest_neighbors()
+        for op_names_list in self.plaquette_obs:
+            obs = "_".join(op_names_list)
+            self.obs_list[obs].get_expval(self.H.Npsi[state_number])
+            self.res[obs] = self.obs_list[obs].avg
         for op_names_list in self.nbody_obs:
             obs = "_".join(op_names_list)
             self.obs_list[obs].get_expval(self.H.Npsi[state_number])
