882ce16c46006462e1b49d77c0650d589d21ace0
Peter Majcen <peter.majcen@pd.infn.it>
2024-10-28 07:44:14 +0100

    implement 2s-mf

---
+++ example_mf_phi4.py
@@ -0,0 +1,61 @@
+# %%
+import numpy as np
+from math import prod
+from ed_lgt.models import phi4_model 
+
+from mean_field import sim
+from time import time
+import logging
+
+logger = logging.getLogger(__name__)
+
+# N eigenvalues
+n_eigs = 1
+# LATTICE GEOMETRY
+lvals = [2]
+dim = len(lvals)
+#directions = "xyz"[:dim]
+n_sites = prod(lvals)
+has_obc = [False]
+d_loc=10
+loc_dims = np.array([d_loc for _ in range(n_sites)])
+#parameters
+par={"lvals": lvals,
+     "has_obc": has_obc,
+     "n_max": d_loc-1
+     }
+
+
+# ACQUIRE HAMILTONIAN COEFFICIENTS
+coeffs = {"mu2": -0.2, "lambda": 0.6}
+# ACQUIRE OPERATORS
+
+start = time()
+# CONSTRUCT THE HAMILTONIAN
+model=phi4_model.Phi4Model(**par) 
+model.build_Hamiltonian(coeffs=coeffs)
+
+ 
+simulation=sim(model.H.Ham,error_mean=1e-5,error_dec=1e-16)
+
+#diag=model.H.diagonalize(n_eigs=n_eigs,format="sparse",loc_dims=loc_dims)
+#res={}
+#res["energy"] = model.H.Nenergies
+
+#print(res["energy"]/lvals[0])
+
+
+
+# ---------------------------------------------------------------------------
+# NEAREST NEIGHBOR INTERACTION
+
+# EXTERNAL MAGNETIC FIELD
+
+# ===========================================================================
+# DIAGONALIZE THE HAMILTONIAN
+
+
+# # Dictionary for results
+# res = {}
+# res["energy"] = H.Nenergies
+# ===========================================================================
---
+++ mean_field.py
@@ -0,0 +1,158 @@
+import numpy as np
+from scipy.linalg import svd
+import scipy.sparse as sp
+from scipy.sparse.linalg import svds
+
+def rand_state(d):
+    """
+    Argument:
+    int d, which gives length of state
+    Returns:
+    Random normalized state of length d
+    """
+    state_init = np.random.rand(d)
+    return state_init / np.linalg.norm(state_init)
+
+def decomp_2body(C, m_A, n_A, m_B, n_B, error):
+    """
+    Input
+    A two body operator C.
+
+    Returns:
+    [[A1,B1,],[A2,B2],...]
+    Such that C=∑_i Ai ⊗ Bi 
+
+    """
+    C_reshaped = C.reshape((m_A, n_A, m_B, n_B))
+    C_flat = C_reshaped.transpose(0, 2, 1, 3).reshape(m_A * m_B, n_A * n_B)
+    U, S, Vh = svd(C_flat)
+
+    ops = [
+        [
+            np.sqrt(Si) * U[:, ii].reshape(m_A, m_B),
+            np.sqrt(Si) * Vh[ii, :].reshape(n_A, n_B),
+        ]
+        for ii, Si in enumerate(S)
+        if Si >= error
+    ]
+    return ops
+
+def decomp_2body_sparse(C, m_A, n_A, m_B, n_B, error=1e-16, k=None):
+    """
+    Input
+    Matrix C sparse
+
+    Returns:
+    [[A1,B1,],[A2,B2],...]
+    Such that C=∑_i Ai ⊗ Bi 
+
+    """
+    assert sp.issparse(C), "C must be sparse."
+    
+    # Convert C to a dense matrix temporarily and reshape 
+    # TODO: circumvent to change to dense matrix
+    C_dense = C.toarray().reshape((m_A, n_A, m_B, n_B))
+    C_flat = C_dense.transpose(0, 2, 1, 3).reshape(m_A * m_B, n_A * n_B)
+    U, S, Vh = svds(sp.csr_matrix(C_flat), k=k if k else min(C_flat.shape) - 1)
+    
+    # order of scipy.sparse.linalg.svds is ascending
+    U, S, Vh = U[:, ::-1], S[::-1], Vh[::-1, :]
+    
+    ops = []
+    for ii, Si in enumerate(S):
+        if Si >= error:
+            A_i = sp.csr_matrix(np.sqrt(Si) * U[:, ii].reshape(m_A, m_B))
+            B_i = sp.csr_matrix(np.sqrt(Si) * Vh[ii, :].reshape(n_A, n_B))
+            ops.append([A_i, B_i])
+
+    return ops
+
+
+def Ham_eff(ops :list, state):
+
+    """
+    Assuming a Hamiltonian H= ∑ H_{i,i+1}
+    Step 1:
+    With an SVD decomposition we find A_i, B_i, 
+    such that H_{i,i+1}=∑ A_i ⊗ B_i.
+
+    Step 2:
+    Do the contraction with the state with:
+    Id ⊗ A_i ⊗ B_i and A_i ⊗ B_i ⊗ Id
+    
+    Step 3:
+    Calculate: H_eff
+
+    Input:
+    Operators and state
+
+    Output: 
+    Effective operator
+    """
+
+    v_L,v_R=0,0 
+    H_l,H_r=0,0
+    H=0
+    Id_A = np.identity(ops[0][0].shape[0])
+    Id_B = np.identity(ops[0][1].shape[0])
+    for Ai, Bi in ops:
+        v_L+= np.inner(state, np.dot(np.kron(Id_B, Ai), state))
+        v_R+= np.inner(state, np.dot(np.kron(Bi, Id_A), state))
+
+    for Ai,Bi in ops:
+        H_l+=v_L*np.kron(Bi,Id_A)
+        H_r+=v_R*np.kron(Id_B,Ai)
+        H+=np.kron(Ai,Bi)
+                         
+    return H_l+H+H_r
+
+
+def test_decomp(ops_decomp,ops,atol=1e-13, rtol=1e-13):
+
+    C_rec = 0
+    for A,B in ops_decomp:
+        C_rec += np.kron(A.toarray(), B.toarray())
+    assert np.allclose(ops.toarray(), C_rec, atol=1e-13, rtol=1e-13)
+
+
+
+
+def sim(ops,error_mean,error_dec):
+    
+    """
+    Step 1:
+    Decompose operators
+    
+    """
+    d_loc=10
+    ops_decomp=decomp_2body_sparse(ops, m_A=d_loc, n_A=d_loc, m_B=d_loc, n_B=d_loc, error=error_dec)
+
+    #test decomp
+    test_decomp(ops_decomp,ops,atol=1e-13, rtol=1e-13)
+    
+    
+    # #get random state
+    state=rand_state(d_loc**2)
+
+    # diff=1+error_mean
+    # E=[10]
+    # ii=1
+    # while diff>error_mean:
+
+    #     #contraction
+    #     H_eff=Ham_eff(ops=ops_decomp,state=state)
+        
+    #     #diagonalize
+    #     Eg=1
+        
+    #     E.append(Eg)
+    #     diff=abs(E[ii]-E[ii-1])
+    #     ii+=1
+
+    # res={"E_conv":E,
+    #      "state":state
+    #      }
+
+    # return res 
+
+
