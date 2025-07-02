SU2 theta term update 1) add Z2 nbody symmetry for SU2 pure theory PBC 2) fix SU2 3D operators 3) increment precision of get_state_configurations

ed_lgt/modeling/qmb_state.py
@@ -318,8 +318,8 @@ class QMB_state:
         # 5) print
         for _, config, amp in zip(idx, cfgs, vals):
             # rescale all the amplitudes to have the first one real and positive
-            rescaled_amp = round(amp * np.exp(-1j * np.angle(vals[0])), 6)
-            square_amp = round(np.abs(amp) ** 2, 6)
+            rescaled_amp = round(amp * np.exp(-1j * np.angle(vals[0])), 8)
+            square_amp = round(np.abs(amp) ** 2, 8)
             coords = " ".join(f"{c:2d}" for c in config)
             logger.info(f"[{coords}] psi={rescaled_amp} |psi|^2={square_amp}")
 

ed_lgt/models/SU2_model.py
@@ -1,7 +1,8 @@
 import numpy as np
+from numba import typed
 from .quantum_model import QuantumModel
 from ed_lgt.modeling import LocalTerm, TwoBodyTerm, PlaquetteTerm
-from ed_lgt.modeling import check_link_symmetry, staggered_mask
+from ed_lgt.modeling import check_link_symmetry, staggered_mask, get_origin_surfaces
 from ed_lgt.operators import (
     SU2_dressed_site_operators,
     SU2_gauge_invariant_states,
@@ -62,12 +63,56 @@ class SU2_Model(QuantumModel):
         ]
         link_sectors = [0 for _ in self.directions]
         # -------------------------------------------------------------------------------
+        # SU2 ELECTRIC-FLUX “NBODY” SYMMETRIES
+        # only in the pure (no-matter) theory, more than 1D, *and* PBC
+        # Constrain, for each cartesian direction, the parity of the face/line through the origin:
+        # 3D:
+        #    for 'Ex' → the yz-face at x=0
+        #    for 'Ey' → the xz-face at y=0
+        #    for 'Ez' → the xy-face at z=0
+        # 2D:
+        #    for 'Ex' → the y-axis at x=0
+        #    for 'Ey' → the x-axis at y=0
+        if self.pure_theory and not any(self.has_obc):
+            logger.info("fixing surface parity fluxes")
+            # one flux‐constraint per cartesian direction
+            nbody_sectors = np.ones(self.dim, dtype=float)
+            nbody_ops = []
+            nbody_sites_list = typed.List()
+            surfaces = get_origin_surfaces(self.lvals)
+            nbody_sym_type = "Z"
+            if self.dim == 2:
+                # in 2D we have two lines through (0,0):
+                line_of = {"x": "y", "y": "x"}
+                for dir in self.directions:
+                    sites = np.array(surfaces[line_of[dir]][1], dtype=np.uint8)
+                    nbody_sites_list.append(sites)
+                    nbody_ops.append(self.ops[f"P_p{dir}"])
+            elif self.dim == 3:
+                # in 3D we have three faces through (0,0,0):
+                face_of = {"x": "yz", "y": "xz", "z": "xy"}
+                for dir in self.directions:
+                    sites = np.array(surfaces[face_of[dir]][1], dtype=np.uint8)
+                    nbody_sites_list.append(sites)
+                    logger.debug(f"{dir} sites: {sites} {surfaces[face_of[dir]][0]}")
+                    nbody_ops.append(self.ops[f"P_p{dir}"])
+        else:
+            # no electric‐flux constraint in 1D, or in OBC or with matter
+            nbody_sectors = None
+            nbody_ops = None
+            nbody_sites_list = None
+            nbody_sym_type = None
+        # -------------------------------------------------------------------------------
         # GET SYMMETRY SECTOR
         self.get_abelian_symmetry_sector(
             global_ops=global_ops,
             global_sectors=global_sectors,
             link_ops=link_ops,
             link_sectors=link_sectors,
+            nbody_ops=nbody_ops,
+            nbody_sectors=nbody_sectors,
+            nbody_sites_list=nbody_sites_list,
+            nbody_sym_type=nbody_sym_type,
         )
         self.default_params()
 

ed_lgt/symmetries/symmetry_sector.py
@@ -226,6 +226,46 @@ def check_nbody_sym_partial(config, sym_op_diags, sym_sectors, nsites_list):
 
 
 @njit(cache=True)
+def check_Z2_nbody_sym_partial(config, sym_op_diags, sym_sectors, nsites_list):
+    """
+    Partial check of Z2 nbody symmetries for a configuration.
+    NOTE: All the nbody symmetries are supposed to be of type U(1) and to involve the same number of sites.
+
+    Args:
+        config (np.array of np.uint16): Partial QMB configuration array.
+        sym_op_diags (np.array of floats): 3D array with shape=(n_symmetries, n_sites, max(loc_dims)).
+            Each symmetry applies an operator to n_sites number of sites (the same operator on all the given sites).
+            Each operator is represented by its diagonal on each lattice site.
+        sym_sectors (np.array of floats): 1D array of shape=(n_symmetries) with the sector value of each symmetry.
+        nsites_list (list of np.array of np.uint16): List of 1D arrays.
+            Each array specifies the sites on which the n_ops operators (of each symmetry) act.
+
+    Returns:
+        bool: True if the partial config satisfies the nbody symmetries, False otherwise.
+    """
+    num_sites = config.shape[0]
+    n_symmetries = sym_op_diags.shape[0]
+    # Iterate over all nbody symmetries
+    for sym_idx in range(n_symmetries):
+        site_indices = nsites_list[sym_idx]
+        nsites_per_symmetry = len(site_indices)
+        # Skip symmetries where one or more indices
+        # are out of the current configuration length
+        if np.any(site_indices >= num_sites):
+            continue
+        # Compute the sum for the symmetry condition
+        sum = 1.0
+        for ii in range(nsites_per_symmetry):
+            site_index = site_indices[ii]
+            op_diag = sym_op_diags[sym_idx, site_index, :]
+            sum *= op_diag[config[site_index]]
+        # Check if the sum violates the symmetry sector
+        if not np.isclose(sum, sym_sectors[sym_idx], atol=1e-13):
+            return False
+    return True
+
+
+@njit(cache=True)
 def check_glob_sym_partial(config, sym_op_diags, sym_sectors, sym_type_flag):
     """
     Check if a (partial or complete) QMB state configuration belongs to a global abelian symmetry sector.
@@ -455,6 +495,7 @@ def get_link_sector_configs(
     nbody_op_diags=None,
     nbody_sectors=None,
     nbody_sites_list=None,
+    nbody_sym_type: str = "U",
 ):
     logger.debug("GETTING LINK SECTOR CONFIGURATIONS")
     if not isinstance(link_sectors, np.ndarray):
@@ -466,6 +507,7 @@ def get_link_sector_configs(
     if nbody_op_diags is not None:
         if not isinstance(nbody_sectors, np.ndarray):
             nbody_sectors = np.array(nbody_sectors, dtype=float)
+        nbody_sym_value = 0 if nbody_sym_type == "U" else 1
         sector_configs = iterative_link_sector_configs_plus(
             loc_dims,
             link_op_diags,
@@ -474,6 +516,7 @@ def get_link_sector_configs(
             nbody_op_diags,
             nbody_sectors,
             nbody_sites_list,
+            nbody_sym_value,
         )
     else:
         sector_configs = iterative_link_sector_configs(
@@ -554,6 +597,7 @@ def iterative_link_sector_configs_plus(
     nbody_op_diags,
     nbody_sectors,
     nbody_sites_list,
+    nbody_sym_value,
 ):
     """
     Iteratively compute the configurations belonging to a symmetry sector,
@@ -601,13 +645,20 @@ def iterative_link_sector_configs_plus(
             # Copy the previous configuration and add the new site's state
             configs_next[ii, :site_idx] = configs_prev[prev_config_idx]
             configs_next[ii, site_idx] = new_site_state
-
-            # Perform the checks for link symmetries
-            checks_next[ii] = check_link_sym_partial(
-                configs_next[ii], link_op_diags, link_sectors, pair_list
-            ) and check_nbody_sym_partial(
-                configs_next[ii], nbody_op_diags, nbody_sectors, nbody_sites_list
-            )
+            if nbody_sym_value == 0:
+                # Perform the checks for link symmetries
+                checks_next[ii] = check_link_sym_partial(
+                    configs_next[ii], link_op_diags, link_sectors, pair_list
+                ) and check_nbody_sym_partial(
+                    configs_next[ii], nbody_op_diags, nbody_sectors, nbody_sites_list
+                )
+            else:
+                # Perform the checks for link symmetries
+                checks_next[ii] = check_link_sym_partial(
+                    configs_next[ii], link_op_diags, link_sectors, pair_list
+                ) and check_Z2_nbody_sym_partial(
+                    configs_next[ii], nbody_op_diags, nbody_sectors, nbody_sites_list
+                )
         # Filter configurations for the current site
         configs_prev = configs_next[checks_next]
         num_configs_prev = configs_prev.shape[0]

scripts/script_SU2_spectrum.py
@@ -35,12 +35,29 @@ with run_sim() as sim:
     local_obs += ["E_square"]
     if not model.pure_theory:
         local_obs += [f"N_{label}" for label in ["tot", "single", "pair", "zero"]]
-    # LIST OF PLAQUETTE OPERATORS
-    plaquette_obs = []
-    # DEFINE OBSERVABLES
-    model.get_observables(local_obs)
     for obs in local_obs:
-        sim.res[obs] = np.zeros(model.H.n_eigs, dtype=float)
+        sim.res[obs] = np.zeros(n_eigs, dtype=float)
+    # LIST OF TWOBODY CORRELATORS
+    twobody_obs = []
+    twobody_axes = []
+    # LIST OF PLAQUETTE OPERATORS
+    if model.dim == 2:
+        plaquette_obs = [["C_px,py", "C_py,mx", "C_my,px", "C_mx,my"]]
+    elif model.dim == 3:
+        plaquette_obs = [
+            ["C_px,py", "C_py,mx", "C_my,px", "C_mx,my"],
+            ["C_px,pz", "C_pz,mx", "C_mz,px", "C_mx,mz"],
+            ["C_py,pz", "C_pz,my", "C_mz,py", "C_my,mz"],
+        ]
+    else:
+        plaquette_obs = []
+    for obs_names_list in plaquette_obs:
+        obs = "_".join(obs_names_list)
+        sim.res[obs] = np.zeros(n_eigs, dtype=float)
+    # DEFINE OBSERVABLES
+    model.get_observables(
+        local_obs, twobody_obs, plaquette_obs, twobody_axes=twobody_axes
+    )
     # QUENCH STATE FOR OVERLAP
     if sim.par["observables"]["get_overlap"]:
         name = sim.par["hamiltonian"]["state"]
@@ -72,12 +89,15 @@ with run_sim() as sim:
         # MEASURE OBSERVABLES
         if sim.par["observables"]["measure_obs"]:
             model.measure_observables(ii)
-            sim.res["N_single"][ii] = stag_avg(model.res["N_single"])
-            sim.res["N_pair"][ii] += 0.5 * stag_avg(model.res["N_pair"], "even")
-            sim.res["N_pair"][ii] += 0.5 * stag_avg(model.res["N_zero"], "odd")
-            sim.res["N_zero"][ii] += 0.5 * stag_avg(model.res["N_zero"], "even")
-            sim.res["N_zero"][ii] += 0.5 * stag_avg(model.res["N_pair"], "odd")
-            sim.res["N_tot"][ii] = sim.res["N_single"][ii] + 2 * sim.res["N_pair"][ii]
+            if not model.pure_theory:
+                sim.res["N_single"][ii] = stag_avg(model.res["N_single"])
+                sim.res["N_pair"][ii] += 0.5 * stag_avg(model.res["N_pair"], "even")
+                sim.res["N_pair"][ii] += 0.5 * stag_avg(model.res["N_zero"], "odd")
+                sim.res["N_zero"][ii] += 0.5 * stag_avg(model.res["N_zero"], "even")
+                sim.res["N_zero"][ii] += 0.5 * stag_avg(model.res["N_pair"], "odd")
+                sim.res["N_tot"][ii] = (
+                    sim.res["N_single"][ii] + 2 * sim.res["N_pair"][ii]
+                )
         # ---------------------------------------------------------------------------
         # OVERLAPS with the INITIAL STATE
         if sim.par["observables"]["get_overlap"]: